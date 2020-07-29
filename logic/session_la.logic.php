7c41c80c07573810c72bdf034b6a761daa996f6b
Ansgar <merkmich@gmail.com>
2020-07-29 22:04:06 +0200

    Rechtschreibfehler korrigiert

--- logic/session_la.logic.php
+++ logic/session_la.logic.php
@@ -2,7 +2,7 @@
 //Dies hier muss in jeder geschützten Seite direkt unterhalb von first.logic.php eingefügt werden!
 if(!isset($_SESSION['la_id'])) {
   $redirect = db::escape($_SERVER['REQUEST_URI']); //Damit man nach dem Login direkt auf die gewünschte Seite geführt wird
-  Form::affirm("Bitte zuerste einloggen");
+  Form::affirm("Bitte zuerst einloggen");
   header('Location: ../ligacenter/lc_login.php?redirect=' . $redirect);
   die();
 }
