cc8ed5de7c70e6ce65c3c5b1c499e67c50d9ce32
Ansgar <merkmich@gmail.com>
2020-08-01 13:52:36 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php 
 //Weiterleitung auf Landingpage
 header("Location: /liga/neues");
+die();
 ?>
-<!--<div style="margin:50px">
+<div style="margin:50px">
     <img src="bilder/logo_lang_small.png" style="max-width: 400px" alt="Ligalogo">
-    <h2>Die Webseite der Deutschen Einradhockeyliga befindet sich gerade im Umzug.</h2>
-    <p>Wir werden in den nächsten Stunden so bald wie möglich wieder online sein.</p>
-</div>-->
+    <h2>Die Webseite der Deutschen Einradhockeyliga befindet sich gerade im Wartungsmodus.</h2>
+    <p>Wir werden so bald wie möglich wieder online sein.</p>
+</div>
