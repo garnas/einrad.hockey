c858ea2153ace9cb4cad22601561535b1344d622
Philipp Gross <pka.gross@web.de>
2020-08-30 12:15:01 +0200

    Layout Ergebnisübersicht angepasst TODO Liste hinzugefügt

--- public/liga/spielplan.php
+++ public/liga/spielplan.php
@@ -1,3 +1,8 @@
+<!-- 
+TODO:
+- Datum und Ort des Turniers in den "Spielplan" einfügen
+- Title entsprechend den anderen anpassen
+-->
 <?php
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LOGIK////////////////////////////////////
@@ -16,11 +21,10 @@ if(empty($akt_turnier->daten)){
 }
 $spielplan = new Spielplan($turnier_id);
 $spielplan->create_spielplan_jgj();
-//$spielplan->update_spiel(1, 3,4,NULL,NULL);
 $tabelle=$spielplan->get_turnier_tabelle();
 $teamliste=$spielplan->teamliste;
 $spielliste=$spielplan->get_spiele();
-//db::debug($teamliste);
+
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
@@ -28,84 +32,89 @@ $titel = "Spielplan";
 
 include '../../templates/header.tmp.php';
 ?>
-<div  class="w3-white w3-container w3-card-4">
-<h3>Spielplan:</h3>
-    <div class="w3-responsive w3-section ">
-        <table class="w3-table w3-striped ">
+<div class="">
+<h1 class="w3-text-primary w3-border-primary">Spielplan</h1>
+
+<!-- TEAMLISTE -->
+<h3 class="w3-text-secondary w3-margin-top">Teamliste</h3>
+    <div class="w3-responsive w3-card">
+        <table class="w3-table w3-striped">
             <tr class="w3-primary">
-                <th>Team ID</th>
-                <th >Teamname</th>
-                <th >Teamblock</th>
-                <th >Wertigkeit</th>
+                <th class="w3-right-align">Team ID</th>
+                <th>Teamname</th>
+                <th class="w3-right-align">Teamblock</th>
+                <th class="w3-right-align">Wertigkeit</th>
             </tr>
 
         <?php foreach ($teamliste as $index => $team){?>
             <tr>
-            <td><?= $team["team_id"]?></td>
+            <td class="w3-right-align"><?= $team["team_id"]?></td>
             <td><?= $team["teamname"]?></td>
-            <td><?= $team["tblock"]?></td>
-            <td><?= $team["wertigkeit"]?></td>
+            <td class="w3-right-align"><?= $team["tblock"]?></td>
+            <td class="w3-right-align"><?= $team["wertigkeit"]?></td>
             </tr>
         <?php }//end foreach?>
         </table>
     </div>
-    <div class="w3-responsive w3-section">
+
+<!-- SPIELE -->
+<h3 class="w3-text-secondary w3-margin-top">Spiele</h3>
+    <div class="w3-responsive w3-card">
         <table class="w3-table w3-striped ">
             <tr class="w3-primary">
-                <th>Zeit</th>
-                <th colspan="2">Schiri</th>
-                
-                <th >Mannschaft 1</th>
-                <th>Mannschaft 2</th>
-                <th>Tore 1</th>
-                <th>Tore 2</th>
-                <th>Pen. 1</th>
-                <th>Pen. 2</th>
+                <th class="w3-right-align">Zeit</th>
+                <th colspan="2" class="w3-center">Schiri</th>
+                <th>Team 1</th>
+                <th>Team 2</th>
+                <th colspan="3" class="w3-center">Ergebnis</th>
+                <th colspan="3" class="w3-center">Penalty</th>
             </tr>
             <?php foreach ($spielliste as $index => $spiel){?>
                 <tr>
-                <td><?=$spiel["zeit"]?></td>
-                <td><?=$spiel["schiri_team_id_a"]?></td>
-                <td><?=$spiel["schiri_team_id_b"]?></td>
+                <td class="w3-right-align"><?=$spiel["zeit"]?></td>
+                <td class="w3-right-align"><?=$spiel["schiri_team_id_a"]?></td>
+                <td class="w3-right-align"><?=$spiel["schiri_team_id_b"]?></td>
                 <td><?=$spiel["team_a_name"]?></td>
                 <td><?=$spiel["team_b_name"]?></td>
-                <td><?=$spiel["tore_a"]?></td>
-                <td><?=$spiel["tore_b"]?></td>
-                <td><?=$spiel["penalty_a"]?></td>
-                <td><?=$spiel["penalty_b"]?></td>
+                <td class="w3-right-align" style="padding-right: 0;"><?=$spiel["tore_a"]?></td>
+                <td class="w3-center" style="padding-left: 0; padding-right: 0;">:</td>
+                <td class="w3-left-align" style="padding-left: 0;"><?=$spiel["tore_b"]?></td>
+                <td class="w3-right-align" style="padding-right: 0;"><?=$spiel["penalty_a"]?></td>
+                <td class="w3-center" style="padding-left: 0; padding-right: 0;">:</td>
+                <td class="w3-left-align" style="padding-left: 0;"><?=$spiel["penalty_b"]?></td>
                 </tr>
             <?php }//end foreach?>
         </table>
    </div>
-   <div class="w3-responsive w3-section">
+
+<!-- ABSCHLUSSTABELLE -->
+<h3 class="w3-text-secondary w3-margin-top">Abschlusstabelle</h3>
+   <div class="w3-responsive w3-card">
     <table class="w3-table w3-striped ">
         <tr class="w3-primary">
-            <th>Platzierung</th>
+            <th class="w3-right-align">Pl.</th>
             <th>Mannschaft</th>
-            <th >Spiele</th>
-            <th >Punkte</th>
-            <th >Tore</th>
-            <th >Gegentore</th>
-            <th >Differenz</th>
-            <th>Ligapunkte</th>
+            <th class="w3-right-align">Spiele</th>
+            <th class="w3-right-align">Punkte</th>
+            <th class="w3-right-align">Tore</th>
+            <th class="w3-right-align">Gegentore</th>
+            <th class="w3-right-align">Differenz</th>
+            <th class="w3-right-align">Ligapunkte</th>
         </tr>
         <?php foreach ($tabelle as $index => $table){?>
             <tr>
-            <td><?=$index+1?></td>
+            <td class="w3-right-align"><?=$index+1?></td>
             <td><?=$table["teamname"]?></td>
-            <td><?=$table["spiele"]?></td>
-            <td><?=$table["punkte"]?></td>
-            <td><?=$table["tore"]?></td>
-            <td><?=$table["gegentore"]?></td>
-            <td><?=$table["diff"]?></td>
-            <td><?=$table["ligapunkte"]?></td>
+            <td class="w3-right-align"><?=$table["spiele"]?></td>
+            <td class="w3-right-align"><?=$table["punkte"]?></td>
+            <td class="w3-right-align"><?=$table["tore"]?></td>
+            <td class="w3-right-align"><?=$table["gegentore"]?></td>
+            <td class="w3-right-align"><?=$table["diff"]?></td>
+            <td class="w3-right-align"><?=$table["ligapunkte"]?></td>
             </tr>
             <?php }//end foreach?>
         </table>
    </div>
-
-
-
-
-
 </div>
+<?php
+include '../../templates/footer.tmp.php';
