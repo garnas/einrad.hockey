1a1c563cc6d8569670cee24610d13cdd38e8ec02
Ansgar <merkmich@gmail.com>
2020-09-01 15:49:05 +0200

    Hinweise zur Ergebnisübermittlung hinzugefügt

--- public/teamcenter/tc_spielplan.php
+++ public/teamcenter/tc_spielplan.php
@@ -99,6 +99,7 @@ include '../../templates/spielplan_vorTurnierTabelle.tmp.php';
     <p>
         <input type="submit" name="gesendet_turnierergebnisse" class="w3-block w3-button w3-tertiary" value="Ergebnisse speichern">
     </p>
+   <?php if($teamcenter && $akt_turnier->daten['phase'] == 'ergebnis'){?><p class="w3-text-green">Dem Ligaausschuss liegt ein Turnierergebniss vor. Durch erneutes speichern, kann das Turnierergebnis verändert werden.</p><?php }//endif?>
 </form>
 
 <?php
--- templates/spielplan_ergebnisTabelle.tmp.php
+++ templates/spielplan_ergebnisTabelle.tmp.php
@@ -1,28 +1,28 @@
 <!-- ABSCHLUSSTABELLE -->
 <h3 class="w3-text-secondary w3-margin-top">Abschlusstabelle</h3>
    <div class="w3-responsive w3-card">
-    <table class="w3-table w3-striped ">
-        <tr class="w3-primary">
-            <th class="w3-right-align">Pl.</th>
-            <th>Team</th>
-            <th class="w3-right-align">Spiele</th>
-            <th class="w3-right-align">Punkte</th>
-            <th class="w3-right-align">Tore</th>
-            <th class="w3-right-align">Gegentore</th>
-            <th class="w3-right-align">Differenz</th>
-            <th class="w3-right-align">Ligapunkte</th>
-        </tr>
-        <?php foreach ($tabelle as $index => $table){?>
-            <tr>
-            <td class="w3-right-align"><?=$index+1?></td>
-            <td><?=$table["teamname"]?></td>
-            <td class="w3-right-align"><?=$table["spiele"]?></td>
-            <td class="w3-right-align"><?=$table["punkte"]?></td>
-            <td class="w3-right-align"><?=$table["tore"]?></td>
-            <td class="w3-right-align"><?=$table["gegentore"]?></td>
-            <td class="w3-right-align"><?=$table["diff"]?></td>
-            <td class="w3-right-align"><?=$table["ligapunkte"]?></td>
+        <table class="w3-table <?php if($teamcenter && $akt_turnier->daten['phase'] == 'ergebnis'){?>w3-pale-green<?php }else{?>w3-striped<?php }//endif?>">
+            <tr class="w3-primary">
+                <th class="w3-right-align">Pl.</th>
+                <th>Team</th>
+                <th class="w3-right-align">Spiele</th>
+                <th class="w3-right-align">Punkte</th>
+                <th class="w3-right-align">Tore</th>
+                <th class="w3-right-align">Gegentore</th>
+                <th class="w3-right-align">Differenz</th>
+                <th class="w3-right-align">Ligapunkte</th>
             </tr>
+            <?php foreach ($tabelle as $index => $table){?>
+                <tr>
+                    <td class="w3-right-align"><?=$index+1?></td>
+                    <td><?=$table["teamname"]?></td>
+                    <td class="w3-right-align"><?=$table["spiele"]?></td>
+                    <td class="w3-right-align"><?=$table["punkte"]?></td>
+                    <td class="w3-right-align"><?=$table["tore"]?></td>
+                    <td class="w3-right-align"><?=$table["gegentore"]?></td>
+                    <td class="w3-right-align"><?=$table["diff"]?></td>
+                    <td class="w3-right-align"><?=$table["ligapunkte"]?></td>
+                </tr>
             <?php }//end foreach?>
         </table>
    </div>
