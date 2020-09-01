90c688c3c6381df9eaaad24eeaf89b50a1e6bf4b
Ansgar <merkmich@gmail.com>
2020-09-01 16:15:24 +0200

    header und form warnungen umstrukturiert, damit diese nciht mehrfach oder nciht angezeigt werden

--- public/teamcenter/tc_spielplan.php
+++ public/teamcenter/tc_spielplan.php
@@ -43,23 +43,27 @@ if(isset($_POST["gesendet_tur"])){
 $tabelle=$spielplan->get_turnier_tabelle();
 $teamliste=$spielplan->teamliste;
 $spielliste=$spielplan->get_spiele();
-$penalty_warning=$spielplan->penalty_warning;
-if(empty($penalty_warning)){
-    $penalty_warning=" Kein Penalty notwendig";
-}else{
-    Form::attention($penalty_warning);
-}
+
 //Turnierergebnisse speichern
 //TODO nach Datum testen, ist es später ale Turnier begin und nicht merh als X Tage nach Turneir??
 if(isset($_POST["gesendet_turnierergebnisse"])){
     //Sind alle spiele gespielt und kein Penalty mehr notwendig
     $spielplan->set_ergebnis($tabelle);
+    header('Location: ' . db::escape($_SERVER['REQUEST_URI']));
+    die();
 }
 
 //Hinweis Kaderkontrolle und Turnierreport
 $turnier_report = new TurnierReport($turnier_id);
 if (!$turnier_report->kader_check()){
     Form::affirm("Bitte kontrolliert die Teamkader und setzt im " . Form::link('../teamcenter/tc_turnier_report.php?turnier_id=' . $turnier_id, 'Turnierreport') . " das entsprechende Häckchen.");
+}
+
+$penalty_warning=$spielplan->penalty_warning;
+if(empty($penalty_warning)){
+    $penalty_warning=" Kein Penalty notwendig";
+}else{
+    Form::attention($penalty_warning);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -71,7 +75,7 @@ include '../../templates/header.tmp.php';
 ?>
 
 <!-- LINK TURNIERREPORT -->
-<p><?=Form::link('../teamcenter/turnier_report.php?turnier_id=' . $turnier_id, '<i class="material-icons">create</i> Turnierreport ausfüllen')?></p>
+<p><?=Form::link('../teamcenter/tc_turnier_report.php?turnier_id=' . $turnier_id, '<i class="material-icons">create</i> Turnierreport ausfüllen')?></p>
 
 <!-- TEAMLISTE -->
 <?php
@@ -96,8 +100,11 @@ include '../../templates/spielplan_vorTurnierTabelle.tmp.php';
     include '../../templates/spielplan_ergebnisTabelle.tmp.php';
 ?>
 <form method="post">
-    <?php if($teamcenter && $akt_turnier->daten['phase'] == 'ergebnis'){?>
+    <?php if($akt_turnier->daten['phase'] == 'ergebnis'){?>
         <p class="w3-text-green">Dem Ligaausschuss liegt ein Turnierergebniss vor. Durch erneutes Speichern kann das Turnierergebnis verändert werden.</p>
+    <?php }//endif?>
+    <?php if($akt_turnier->daten['phase'] != 'ergebnis'){?>
+        <p class="w3-text-grey">Dem Ligaausschuss liegt noch kein Turnierergebniss vor.</p>
     <?php }//endif?>
     <p>
         <input type="submit" name="gesendet_turnierergebnisse" class="w3-block w3-button w3-tertiary" value="Ergebnisse speichern">
