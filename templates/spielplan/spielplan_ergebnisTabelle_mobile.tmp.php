527c6eadbb199d83ac7619dd8f7b88d95441a74f
Ansgar <merkmich@gmail.com>
2020-09-03 12:23:59 +0200

    detailtabelle verbessert

--- templates/spielplan/spielplan_ergebnisTabelle_mobile.tmp.php
+++ templates/spielplan/spielplan_ergebnisTabelle_mobile.tmp.php
@@ -17,41 +17,35 @@
             <?php }//end foreach?>
         </table>
     </div>
-    
     <!-- Modal -->
     <span class="w3-button w3-text-primary" onclick="document.getElementById('tabelle_details_mobile').style.display='block'" ><i class='material-icons'>info</i> Details anzeigen</span>
     <div id="tabelle_details_mobile" class="w3-modal">
         <div class="w3-modal-content w3-card-4">
-            <div class="w3-container">
-                <span onclick="document.getElementById('tabelle_details_mobile').style.display='none'" class="w3-button w3-text-secondary w3-display-topright">&times;</span>
-                <h3 class="w3-text-secondary w3-margin-top">Details</h3>
-                <div class="w3-responsive w3-card w3-section">
-                    <table class="w3-table <?php if($teamcenter && $akt_turnier->daten['phase'] == 'ergebnis'){?>w3-pale-green<?php }else{?>w3-striped<?php }//endif?>">
-                        <tr class="w3-primary">
-                            <th>Pl.</th>
-                            <th>Team</th>
-                            <th class="w3-center">Spiele</th>
-                            <th class="w3-center">Punkte</th>
-                            <th class="w3-center">Tore</th>
-                            <th class="w3-center">Gegentore</th>
-                            <th class="w3-center">Differenz</th>
-                            <th class="w3-center">Ligapunkte</th>
+            <div class="w3-responsive w3-card" style="white-space: nowrap;">
+                <table class="w3-table <?php if($teamcenter && $akt_turnier->daten['phase'] == 'ergebnis'){?>w3-pale-green<?php }else{?>w3-striped<?php }//endif?>">
+                    <tr class="w3-primary">
+                        <th>Pl.</th>
+                        <th>Team</th>
+                        <th class="w3-center">Spiele</th>
+                        <th class="w3-center">Punkte</th>
+                        <th class="w3-center">Tore</th>
+                        <th class="w3-center">Gegentore</th>
+                        <th class="w3-center">Differenz</th>
+                        <th class="w3-center">Ligapunkte</th>
+                    </tr>
+                    <?php foreach ($tabelle as $index => $table){?>
+                        <tr>
+                            <td><?=$index+1?></td>
+                            <td><?=$table["teamname"]?></td>
+                            <td class="w3-center"><?=$table["spiele"]?></td>
+                            <td class="w3-center"><?=$table["punkte"]?></td>
+                            <td class="w3-center"><?=$table["tore"]?></td>
+                            <td class="w3-center"><?=$table["gegentore"]?></td>
+                            <td class="w3-center"><?=$table["diff"]?></td>
+                            <td class="w3-center"><?=$table["ligapunkte"]?></td>
                         </tr>
-                        <?php foreach ($tabelle as $index => $table){?>
-                            <tr>
-                                <td><?=$index+1?></td>
-                                <td><?=$table["teamname"]?></td>
-                                <td class="w3-center"><?=$table["spiele"]?></td>
-                                <td class="w3-center"><?=$table["punkte"]?></td>
-                                <td class="w3-center"><?=$table["tore"]?></td>
-                                <td class="w3-center"><?=$table["gegentore"]?></td>
-                                <td class="w3-center"><?=$table["diff"]?></td>
-                                <td class="w3-center"><?=$table["ligapunkte"]?></td>
-                            </tr>
-                        <?php }//end foreach?>
-                    </table>
-                </div>
-                <p class="w3-text-grey"><i>(Faktor 6/<?=$spielzeit['faktor']?>)</i></p>
+                    <?php }//end foreach?>
+                </table>
             </div>
         </div>
     </div>
