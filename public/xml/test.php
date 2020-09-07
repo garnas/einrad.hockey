57cf1423ba233435cf88b739661802d4f9ab2b73
Ansgar <merkmich@gmail.com>
2020-09-07 17:18:41 +0200

    Create test.php

---
+++ public/xml/test.php
@@ -0,0 +1,29 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LOGIK////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+require_once '../../logic/first.logic.php'; //autoloader und Session
+
+//Assoziatives Array aller Turniere der Aktuellen Saison
+$turniere = Turnier::get_all_turniere("WHERE saison='".Config::SAISON."'");
+
+//db::debug wird verwendet fürs debugging. Es zeigt immer den Inhalt von Variablen oben auf der aufgerufenen Seite auf.
+db::debug($turniere);
+
+//Assoziatives Array aller Teamdaten
+$teams = Team::get_all_teamdata();
+db::debug($teams);
+
+//Datum-Umwandlung auf Deutsch:
+//strtotime erstellt aus einem String eine Unix-Zeit
+$datum = strftime("%A, %d.%m.%Y %H:%M&nbsp;Uhr", strtotime("2020-11-12"));
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LAYOUT///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+include '../../templates/header.tmp.php';
+?>
+<h1>Oben werden Beispiel Arrays dargestellt</h1>
+<p>Beispieldatum: <?=$datum?></p>
+
+<?php include '../../templates/footer.tmp.php';
