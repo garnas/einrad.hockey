202432ba7463d2d9089ff9719ef53470bcc9cc5e
Largofin <pka.gross@web.de>
2020-11-06 17:31:07 +0100

    Notwendige Funktionen erstellt

---
+++ classes/challenge.class.php
@@ -0,0 +1,41 @@
+<?php
+
+class Challenge {
+    function get_teams(){
+        $sql = "
+        SELECT teamname, COUNT(sp.spieler_id) AS mitglieder, COUNT(ch.id) AS einträge, ROUND(SUM(kilometer), 1) AS kilometer
+        FROM `oeffi_challenge` ch, spieler sp, teams_liga t
+        WHERE ch.spieler_id = sp.spieler_id
+        AND sp.team_id = t.team_id
+        GROUP BY teamname
+        ORDER BY kilometer DESC
+        ";
+        $result = db::readdb($sql);
+
+        return db::escape(mysqli_fetch_assoc($result));
+    }
+
+    function get_spieler(){
+        $sql = "
+        SELECT sp.vorname, t.teamname, COUNT(ch.id) AS einträge, ROUND(SUM(kilometer), 1) AS kilometer
+        FROM `oeffi_challenge` ch, spieler sp, teams_liga t
+        WHERE ch.spieler_id = sp.spieler_id
+        AND sp.team_id = t.team_id
+        GROUP BY sp.spieler_id
+        ORDER BY kilometer DESC
+        ";
+        $result = db::readdb($sql);
+
+        return db::escape(mysqli_fetch_assoc($result));
+    }
+
+    function set_data(spieler, distanz, datum){
+        $sql = "
+        INSERT INTO `oeffi_challenge`(`spieler_id`, `kilometer`, `datum`) VALUES (spieler, distanz, datum)
+        ";
+        $result = db::writedb($sql);
+    }
+
+}
+
+?>
