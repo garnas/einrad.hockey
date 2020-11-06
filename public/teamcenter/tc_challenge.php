1ab41b98e31d499ffcc133a8cfe3fe8f9851920f
Largofin <pka.gross@web.de>
2020-11-06 22:08:27 +0100

    Formular für Eingabe erstellt

---
+++ public/teamcenter/tc_challenge.php
@@ -0,0 +1,45 @@
+    <?php
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LOGIK////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+require_once '../../logic/first.logic.php'; //autoloader und Session
+require_once '../../logic/session_team.logic.php'; //Auth
+
+$team_id = $_SESSION["team_id"];
+$kader = Spieler::get_teamkader($team_id);
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LAYOUT///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+include '../../templates/header.tmp.php';
+?>
+
+<h1 class="w3-text-primary">Kilometer-Challenge</h1>
+<!-- <h2 class="w3-text-grey">Eintragen von Ergebnissen</h2> -->
+
+<form method="post">
+    <div class="w3-panel w3-card-4">
+        <h3>Eintragen</h3>
+        <p>
+            <label for="spieler">Spieler/in</label>
+            <select required id="spieler" name="spieler">
+                <?php foreach($kader as $spieler){?>
+                    <option><?=$spieler["vorname"]?> <?=$spieler["nachname"]?></option>
+                <?php } ?>
+            </select>
+        </p>
+        <p>
+            <label for="kilometer">Strecke</label>
+            <input required type="number" id="kilometer" name="kilometer" min="0" step="0.1"></input>
+        </p>
+        <p>
+            <label for="datum">Datum</label>
+            <input required type="date" id="datum" min="<?php date("Y-m-d") ?>" name="datum"></input>
+        </p>
+            <input type="submit" value="submit">
+        </p>
+    </div>
+</form>
+
+<?php
+include '../../templates/footer.tmp.php';
