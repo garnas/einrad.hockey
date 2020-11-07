157901cb862c4d6ed553387407afd8c14c7e736b
Largofin <pka.gross@web.de>
2020-11-07 18:23:33 +0100

    Fertigstellung TC

--- public/teamcenter/tc_challenge.php
+++ public/teamcenter/tc_challenge.php
@@ -9,6 +9,13 @@ require_once '../../logic/challenge.logic.php';
 $team_id = $_SESSION["team_id"];
 $kader = Spieler::get_teamkader($team_id);
 
+foreach($teamliste as $team) { 
+    if ($team["team_id"] == $team_id) {
+        $platz = $team["platz"];
+        $kilometer = $team["kilometer"];
+    }
+}
+
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
@@ -17,6 +24,15 @@ include '../../templates/header.tmp.php';
 
 <h1 class="w3-text-primary">Kilometer-Challenge</h1>
 <!-- <h2 class="w3-text-grey">Eintragen von Ergebnissen</h2> -->
+
+<div class="w3-panel w3-card-4">
+    <p class="w3-text-primary w3-half w3-center w3-xxxlarge">
+        #<?=$platz ?>
+    </p>
+    <p class="w3-text-primary w3-half w3-center w3-xxxlarge">
+        <?=number_format($kilometer, 1, ',', '.'); ?> km
+    </p>
+</div>
 
 <form method="post">
     <div class="w3-panel w3-card-4">
@@ -37,12 +53,38 @@ include '../../templates/header.tmp.php';
         <p>
             <label class="w3-text-primary" for="datum">Datum</label>
             <input required class="w3-input w3-border w3-border-primary" type="date" id="datum" min="<?php date("Y-m-d") ?>" name="datum"></input>
-            <i class="w3-text-grey">Das frühstmögliche Datum ist der XX.XX.2020.</i>
+            <i class="w3-text-grey">Das Datum muss zwischen dem <?=$start ?> und <?=$end ?> liegen.</i>
         </p>
-            <input type="submit" name="put_challenge" value="Eintragen!">
+            <input type="submit" name="put_challenge" value="Eintragen!" class="w3-secondary w3-button w3-block">
         </p>
     </div>
 </form>
 
+<div class="w3-hide-small">
+    <div class="w3-responsive w3-card">
+        <table class="w3-table w3-striped">
+            <tr class="w3-primary">
+                <th class="w3-center">Platzierung</th>
+                <th colspan="2" class="w3-center">Spieler/in</th>
+                <th class="w3-center">Einträge</th>
+                <th class="w3-center">Kilometer</th>
+            <tr>
+            <?php foreach ($spielerliste as $spieler) {
+                if ($spieler["team_id"] == $team_id) {
+            ?> 
+                <tr>
+                    <td class="w3-center"><?=$spieler["platz"]?></td>
+                    <td class="w3-center"><?=$spieler['vorname']?></td>
+                    <td class="w3-center"><?=$spieler['nachname']?></td>
+                    <td class="w3-center"><?=$spieler['einträge']?></td>
+                    <td class="w3-center"><?=number_format($spieler['kilometer'], 1, ',', '.');?></td>
+                </tr>
+            <?php 
+                } //end if
+            } //end foreach 
+            ?>
+        </table>
+    </div>
+</div>
 <?php
 include '../../templates/footer.tmp.php';
