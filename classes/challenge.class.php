a3c09699ea5e8d3647041e25113729605c7bbb1e
Largofin <pka.gross@web.de>
2020-11-16 23:01:12 +0100

    Teamfunktionen hinzugefügt

--- classes/challenge.class.php
+++ classes/challenge.class.php
@@ -60,6 +60,8 @@ class Challenge {
         SELECT ch.id, sp.team_id, ch.datum, sp.vorname, sp.nachname, ch.kilometer, ch.radgröße
         FROM `oeffi_challenge` ch, `spieler` sp
         WHERE ch.spieler_id = sp.spieler_id
+        AND datum >= '" . date("Y-m-d", strtotime($this->challenge_start)) . "'
+        AND datum <= '" . date("Y-m-d", strtotime($this->challenge_end)) . "'
         AND ch.count = TRUE
         ORDER BY ch.datum DESC, ch.timestamp DESC
         ";
@@ -73,6 +75,53 @@ class Challenge {
         return db::escape($daten);
     }
     
+    function get_team_eintraege($team_id) {
+        $sql = "
+        SELECT ch.id, sp.team_id, ch.datum, sp.vorname, sp.nachname, ch.kilometer, ch.radgröße
+        FROM `oeffi_challenge` ch, `spieler` sp
+        WHERE ch.spieler_id = sp.spieler_id
+        AND datum >= '" . date("Y-m-d", strtotime($this->challenge_start)) . "'
+        AND datum <= '" . date("Y-m-d", strtotime($this->challenge_end)) . "'
+        AND sp.team_id = '" . $team_id . "'
+        AND ch.count = TRUE
+        ORDER BY ch.datum DESC, ch.timestamp DESC
+        ";
+        $result = db::readdb($sql);
+
+        $daten = [];
+        while($row = mysqli_fetch_assoc($result)){
+            array_push($daten, $row);
+        }
+
+        return db::escape($daten);
+    }
+
+    function get_team_spieler($team_id) {
+        $sql = "
+        SELECT platz, vorname, kilometer
+        FROM (
+            SELECT ROW_NUMBER() OVER (ORDER BY kilometer DESC) AS platz, sp.team_id, sp.vorname, ROUND(SUM(kilometer), 1) AS kilometer
+            FROM `oeffi_challenge` ch, spieler sp, teams_liga t
+            WHERE ch.spieler_id = sp.spieler_id
+            AND sp.team_id = t.team_id
+            AND datum >= '" . date("Y-m-d", strtotime($this->challenge_start)) . "'
+            AND datum <= '" . date("Y-m-d", strtotime($this->challenge_end)) . "'
+            AND ch.count = TRUE
+            GROUP BY sp.spieler_id
+        ) AS spieler
+        WHERE team_id = '" . $team_id . "'
+        ORDER BY kilometer DESC
+        ";
+        $result = db::readdb($sql);
+
+        $daten = [];
+        while($row = mysqli_fetch_assoc($result)){
+            array_push($daten, $row);
+        }
+
+        return db::escape($daten);
+    }
+
     public static function set_data($spieler, $distanz, $radgroesse, $datum){
         $sql = "
         INSERT INTO `oeffi_challenge`(`spieler_id`, `kilometer`, `radgröße`, `datum`) VALUES ('$spieler', '$distanz', '$radgroesse', '$datum')
