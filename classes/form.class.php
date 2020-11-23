3608af9edb3501e9e452bf6ae0c458ea65a584ea
Ansgar <merkmich@gmail.com>
2020-11-24 00:13:33 +0100

    Sekunden in die Info-Bar eingepasst

--- classes/form.class.php
+++ classes/form.class.php
@@ -156,12 +156,11 @@ class Form
                     <span id='countdown_minutes'>--</span>
                     <span class="w3-small w3-text-grey" style="display: block">Minuten</span>
                 </span>
-                <!--
+                <span class="w3-center" style="display: inline-block">
                     <span class="w3-center" style="display: inline-block">
                     <span id='countdown_seconds'>--</span>
                     <span class="w3-small w3-text-grey" style="display: block">Sekunden</span>
                 </span>
-                -->
             </div>
         <?php
     }
