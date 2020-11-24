59cfc7dd8f269d5a599d0e19b82f824f40ff2e04
Largofin <pka.gross@web.de>
2020-11-24 21:17:54 +0100

    Testanpassungen

--- public/liga/challenge.php
+++ public/liga/challenge.php
@@ -28,11 +28,14 @@ include '../../templates/header.tmp.php';
 </p>
 <a href='../teamcenter/tc_challenge.php' class="w3-button w3-secondary">Kilometer eintragen!</a>
 
-<h3 class="w3-text-secondary w3-margin-top">Ergebnisse</h3>
+<h3 class="w3-text-secondary w3-margin-top">Gesamt-km</h3>
+<p class="w3-text-gray">
+    Schaffen wir es zusammen bis nach Sydney? Zum am weitesten entfernten Einradhockeyteam.
+</p>
 
 <!-- ProgressBar -->
 <div class='w3-row w3-light-grey'>
-    <div class='w3-col w3-tertiary w3-card-2' style='max-width: 100%; width: <?=$percent?>%; padding-right: 5px; text-align: right;'><p><?=number_format($value, 1, ',', '.');?> km</p></div>
+    <div class='w3-col w3-card-2' style='max-width: 100%; width: <?=$percent?>%; padding-right: 5px; text-align: right; background-color: rgb(189, 148, 107);'><p style="margin-top: 5px; margin-bottom: 5px;"><?=number_format($value, 1, ',', '.');?> km</p></div>
 </div>
 
 <p class='w3-text-gray'><span>Berlin</span><span class="w3-right">Sydney</span></p>
@@ -41,43 +44,23 @@ include '../../templates/header.tmp.php';
 <div class="w3-section w3-margin-top">
     <div id='button_da'>
         <button  class="w3-primary w3-block w3-button" onclick='modal("sonderauswertungen_panels");modal("button_da");modal("button_weg");'>
-            <i class="material-icons">keyboard_arrow_down</i> Sonderwertungen <i class="material-icons">keyboard_arrow_down</i>
+            <i class="material-icons">keyboard_arrow_down</i> Nenneswerte FahrerInnen <i class="material-icons">keyboard_arrow_down</i>
         </button>
     </div>
     <div id='button_weg' style='display: none;'>     
         <button class="w3-primary w3-block w3-button" onclick='modal("sonderauswertungen_panels");modal("button_da");modal("button_weg");'>
-            <i class="material-icons">keyboard_arrow_up</i> Sonderwertungen <i class="material-icons">keyboard_arrow_up</i>
+            <i class="material-icons">keyboard_arrow_up</i> Nenneswerte FahrerInnen <i class="material-icons">keyboard_arrow_up</i>
         </button>
     </div>
 </div>
 
 <!-- Kacheln für Sonderauswertungen -->
 <div id='sonderauswertungen_panels' class="w3-row-padding w3-stretch" style='display: none;'>
-    <!-- Alter ist 50+ -->
-    <div class="w3-third">
-        <div class="w3-panel w3-primary w3-card-4">
-        <p class="w3-center">
-            50+
-        </p>
-        <p class="w3-center w3-tiny">
-            Die meisten gefahrenen Kilometer von einer Person älter oder gleich 50 Jahren.
-        </p>
-        <p class="w3-center w3-xxlarge">
-            <?=number_format($alt['kilometer'], 1, ',', '.');?> km
-        </p>
-        <p class="w3-center">
-            <?=$alt['vorname']?><br /><?=$alt['teamname']?>
-        </p>
-        </div>
-    </div>
     <!-- Alter ist U16 -->
     <div class="w3-third">
         <div class="w3-panel w3-primary w3-card-4">
         <p class="w3-center">
             U16
-        </p>
-        <p class="w3-center w3-tiny">
-            Die meisten gefahrenen Kilometer von einer Person unter 16 Jahren.
         </p>
         <p class="w3-center w3-xxlarge">
             <?=number_format($jung['kilometer'], 1, ',', '.');?> km
@@ -87,14 +70,25 @@ include '../../templates/header.tmp.php';
         </p>
         </div>
     </div>
+    <!-- Alter ist 50+ -->
+    <div class="w3-third">
+        <div class="w3-panel w3-primary w3-card-4">
+        <p class="w3-center">
+            50+
+        </p>
+        <p class="w3-center w3-xxlarge">
+            <?=number_format($alt['kilometer'], 1, ',', '.');?> km
+        </p>
+        <p class="w3-center">
+            <?=$alt['vorname']?><br /><?=$alt['teamname']?>
+        </p>
+        </div>
+    </div>
     <!-- Einradhockeyrad -->
     <div class="w3-third">
         <div class="w3-panel w3-primary w3-card-4">
         <p class="w3-center">
             Einradhockey-Rad
-        </p>
-        <p class="w3-center w3-tiny">
-            Die meisten gefahrenen Kilometer von einer Person auf einem Einradhockey-Rad.
         </p>
         <p class="w3-center w3-xxlarge">
             <?=number_format($einradhockey['kilometer'], 1, ',', '.');?> km
@@ -136,8 +130,8 @@ include '../../templates/header.tmp.php';
     </table>
 </div>
 
-
 <h3 class="w3-text-secondary w3-margin-top">Teilnehmer/innen</h3>
+
 <div class="w3-responsive w3-card">
     <table class="w3-table w3-striped">
         <tr class="w3-primary">
