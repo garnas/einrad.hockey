cb9fb8bb9bffd12531a371a008cda62e4c593b52
Ansgar <merkmich@gmail.com>
2020-11-24 01:32:06 +0100

    Radgröße Step zu 0.1
    gab es nicht jemanden mit einem 26.2 zoll rad?

--- public/teamcenter/tc_challenge_eintraege.php
+++ public/teamcenter/tc_challenge_eintraege.php
@@ -38,7 +38,7 @@ include '../../templates/header.tmp.php';
                 </p>
                 <p>
                     <label class="w3-text-primary" for="radgroesse">Radgröße</label>
-                    <input required class="w3-input w3-border w3-border-primary" type="number" id="radgroesse" name="radgroesse" min="0" step="0.5">
+                    <input required class="w3-input w3-border w3-border-primary" type="number" id="radgroesse" name="radgroesse" min="0" step="0.1">
                     <i class="w3-text-grey">Angabe in Zoll.</i>
                 </p>
                 <p>
