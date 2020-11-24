fba0e53033242b81560b68a649da1a27714c581f
Ansgar <merkmich@gmail.com>
2020-11-24 18:55:54 +0100

    Umbrechen der Uhr verhindert

--- classes/form.class.php
+++ classes/form.class.php
@@ -143,7 +143,7 @@ class Form
     public static function countdown($date, $id = 'countdown'){
         ?>
             <script>countdown('<?=date("Y-m-d\TH:i:s", strtotime($date))?>', '<?=$id?>')</script>
-            <div id='countdown' class="w3-xlarge w3-text-primary">
+            <div id='countdown' class="w3-xlarge w3-text-primary" style='white-space: nowrap;'>
                 <span class="w3-center w3-margin-right" style="display: inline-block">
                     <span id='countdown_days'>--</span>
                     <span class="w3-small w3-text-grey" style="display: block">Tage</span>
