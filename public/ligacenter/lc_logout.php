3914a6c1cdfe1aae9b2d9bfbe4c90a9b813ef9da
Ansgar <merkmich@gmail.com>
2020-12-02 17:26:41 +0100

    Besucherstatistiken

--- public/ligacenter/lc_logout.php
+++ public/ligacenter/lc_logout.php
@@ -2,13 +2,13 @@
 require_once '../../logic/first.logic.php'; //autoloader und Session
 require_once '../../logic/session_la.logic.php'; //Auth
 
-if (!isset($_SESSION['team_id'])){
+/* if (!isset($_SESSION['team_id'])){
     session_destroy();
     session_start();
-}else{
+}else{*/
     unset($_SESSION['la_login_name']); 
     unset($_SESSION['la_id']);
-}
+//}
 
 Form::affirm("Logout erfolgreich");
 header('Location: ../liga/neues.php');
