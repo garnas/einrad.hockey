e77f63894d718a30a9757fadf62a7c21c19bb3b3
Philipp Gross <pka.gross@web.de>
2020-12-14 19:28:56 +0100

    Daten in Team-Urkunde eingefügt

---
+++ public/teamcenter/tc_challenge_team_urkunde.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../../logic/first.logic.php'; // Autoloader und Session
+require_once '../../logic/session_team.logic.php'; //Auth
+require_once '../../logic/challenge.logic.php'; // Logic der Challenge
+
+$team_id = 12;
+$urkunden_daten = $challenge->get_team_result($team_id);
+
+?>
+
+<p>Das Team <?=$urkunden_daten['teamname']?> hat mit <?=$urkunden_daten['kilometer']?> den <?=$urkunden_daten['platz']?>. Platz erreicht!</p>
