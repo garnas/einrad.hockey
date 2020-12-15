3eb40224a64e525d2e3e15c8c8fea0e8b61d9b74
Philipp Gross <pka.gross@web.de>
2020-12-15 21:58:31 +0100

    team_id wird an die Urkunde übergeben

--- public/teamcenter/tc_challenge_team_urkunde.php
+++ public/teamcenter/tc_challenge_team_urkunde.php
@@ -3,7 +3,7 @@ require_once '../../logic/first.logic.php'; // Autoloader und Session
 require_once '../../logic/session_team.logic.php'; //Auth
 require_once '../../logic/challenge.logic.php'; // Logic der Challenge
 
-$team_id = 12;
+$team_id = $_GET['team_id'];
 $urkunden_daten = $challenge->get_team_result($team_id);
 
 ?>
