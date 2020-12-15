40e8e2b0c57acf8fa4be12af571647e7faff046e
Philipp Gross <pka.gross@web.de>
2020-12-15 22:01:37 +0100

    Aufschlag für Teamurkunde hinzugefügt

--- public/teamcenter/tc_challenge_team_urkunde.php
+++ public/teamcenter/tc_challenge_team_urkunde.php
@@ -6,6 +6,39 @@ require_once '../../logic/challenge.logic.php'; // Logic der Challenge
 $team_id = $_GET['team_id'];
 $urkunden_daten = $challenge->get_team_result($team_id);
 
-?>
+// Css-Code als String
+ob_start();
+// Es wurde eine eigene Css-Datei für die PDF-Erstellung erstellt, da der Css-Code nicht immer kompatibel mit mpdf war
+include "../css/pdf.css.php";
 
-<p>Das Team <?=$urkunden_daten['teamname']?> hat mit <?=$urkunden_daten['kilometer']?> den <?=$urkunden_daten['platz']?>. Platz erreicht!</p>
+$css_style = ob_get_clean();
+
+// Start der PDF Erstellung
+$mpdf = PDF::start_mpdf();
+
+$html = '
+<div style="height: 50mm;">
+    <div style="float: right; width: 45mm;"><img src="../bilder/logo_kurz_small.png"></div>
+</div>
+<div style="font-size: 16px;">
+    <div style="margin-bottom: 5mm;">Das Team</div>
+    <div class="w3-text-primary" style="font-size: 28px; font-weight: bold; margin-bottom: 10mm;">' . $urkunden_daten['teamname'] . '</div>
+    <div style="">erreichte bei der</div>
+    <div style="margin-bottom: 10mm; font-size: 28px;">km-Challenge 2020</div>
+    <div style="">mit</div>
+    <div style="margin-bottom: 10mm; font-size: 28px;">' . number_format($urkunden_daten['kilometer'], 1, ',', '.') . ' km' . '</div>
+    <div style="">den</div>
+    <div style="font-size: 28px;">' . $urkunden_daten['platz'] . '. Platz' . '</div>
+</div>
+';
+
+// PDF Bearbeitung
+$mpdf->SetTitle('Urkunde km-Challenge 2020');
+// $mpdf->SetHTMLHeader('<img src="../bilder/logo_kurz_small.png" style="width: 15mm; float: right;">');
+// $mpdf->SetHTMLFooter('<p>Das ist ein Text</p>');
+$mpdf->WriteHTML($css_style,\Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
+
+// Output - Option 'D' für Download, 'I' für im Browser anzeigen
+$mpdf->Output('Urkunde.pdf', 'I');
+?>
