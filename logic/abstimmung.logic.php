36aefe35307382d8ee6b7513ca2527d5122a465e
Philipp Gross <pka.gross@web.de>
2020-12-28 17:58:35 +0100

    Prozentwerte und Überprüfung angepasst

--- logic/abstimmung.logic.php
+++ logic/abstimmung.logic.php
@@ -5,31 +5,33 @@ $uhrzeit = strtotime(date('Y-m-d H:i:s'));
 $abschluss = strtotime($abstimmung->ende_der_abstimmung);
 $beginn = strtotime($abstimmung->beginn_der_abstimmung);
 
-$ergebnisse = $abstimmung->get_ergebnisse();
-$abgegebene_stimmen = $ergebnisse['gesamt'];
 
-$tabelle = array(
-    "Winterpause" => array(
-        "antwort" => "Winterpause",
-        "stimmen" => $ergebnisse['winterpause'] ?? 0,
-        "prozent" => round($ergebnisse['winterpause'] / $abgegebene_stimmen * 100, 2) . '%'
-    ),
-    "Sommerpause" => array(
-        "antwort" => "Sommerpause",
-        "stimmen" => $ergebnisse['sommerpause'] ?? 0,
-        "prozent" => round($ergebnisse['sommerpause'] / $abgegebene_stimmen * 100, 2) . '%'
-    ),
-    "Enthaltung" => array(
-        "antwort" => "Enthaltung",
-        "stimmen" => $ergebnisse['enthaltung'] ?? 0,
-        "prozent" => round($ergebnisse['enthaltung'] / $abgegebene_stimmen * 100, 2) . '%'
-    ),
-    "Gesamt" => array(
-        "antwort" => "Gesamt",
-        "stimmen" => $ergebnisse['gesamt'] ?? 0,
-        "prozent" => round($ergebnisse['gesamt'] / $abgegebene_stimmen * 100, 2) . '%'
-    )
-);
+if(isset($_SESSION['la_id'])) {
+    $ergebnisse = $abstimmung->get_ergebnisse();
+    $abgegebene_stimmen = $ergebnisse['gesamt'];
+    $tabelle = array(
+        "Winterpause" => array(
+            "antwort" => "Winterpause",
+            "stimmen" => $ergebnisse['winterpause'] ?? 0,
+            "prozent" => isset($ergebnisse['wintepauser']) && $ergebnisse['winterpause'] != 0 ? round($ergebnisse['winterpauser'] / $abgegebene_stimmen * 100) . '%' : '-'
+        ),
+        "Sommerpause" => array(
+            "antwort" => "Sommerpause",
+            "stimmen" => $ergebnisse['sommerpause'] ?? 0,
+            "prozent" => isset($ergebnisse['sommerpause']) && $ergebnisse['sommerpause'] != 0 ? round($ergebnisse['sommerpause'] / $abgegebene_stimmen * 100) . '%' : '-'
+        ),
+        "Enthaltung" => array(
+            "antwort" => "Enthaltung",
+            "stimmen" => $ergebnisse['enthaltung'] ?? 0,
+            "prozent" => isset($ergebnisse['enthaltung']) && $ergebnisse['enthaltung'] != 0 ? round($ergebnisse['enthaltung'] / $abgegebene_stimmen * 100) . '%' : '-'
+        ),
+        "Gesamt" => array(
+            "antwort" => "Gesamt",
+            "stimmen" => $ergebnisse['gesamt'] ?? 0,
+            "prozent" => ($ergebnisse['gesamt']) != 0 ? round($ergebnisse['gesamt'] / $abgegebene_stimmen * 100) . '%' : '-'
+        )
+    );
+}
 
 if(isset($_SESSION['team_id'])) {
     $team_id = $_SESSION["team_id"];
