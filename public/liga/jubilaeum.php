bb51f2ab230114c3d435e38ceec5d26a6385cf15
Philipp Gross <pka.gross@web.de>
2021-01-03 00:22:34 +0100

    Bilder in Seite eingefügt

--- public/liga/jubilaeum.php
+++ public/liga/jubilaeum.php
@@ -12,6 +12,11 @@ $content = "Spielerprofile für das 25. Jubiläum der Deutschen Einradhockeyliga
 include '../../templates/header.tmp.php';
 ?>
 
-
+<div class="w3-content">
+    <img class="slideshow1" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+    <img class="slideshow1" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+    <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+    <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+</div>
 
 <?php include '../../templates/footer.tmp.php';
