8584f3860f8170d567f674a0aa06202dfe6e3025
Philipp Gross <pka.gross@web.de>
2021-01-03 00:53:37 +0100

    Slides nebeneinander gestellt

--- public/liga/jubilaeum.php
+++ public/liga/jubilaeum.php
@@ -12,24 +12,23 @@ $content = "Spielerprofile für das 25. Jubiläum der Deutschen Einradhockeyliga
 include '../../templates/header.tmp.php';
 ?>
 
-<div class="w3-content w3-display-container">
-    <img class="slideshow1" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
-    <img class="slideshow1" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
-    <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
-    <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
-
-    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 0)">&#10094;</button>
-    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 0)">&#10095;</button>
-</div>
-
-<div class="w3-content w3-display-container">
-    <img class="slideshow2" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
-    <img class="slideshow2" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
-    <img class="slideshow2" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
-    <img class="slideshow2" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
-
-    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
-    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
+<div class="w3-row">
+    <div class=" w3-third w3-display-container">
+      <img class="slideshow1" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+      <img class="slideshow1" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+      <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+      <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+      <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 0)">&#10094;</button>
+      <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 0)">&#10095;</button>
+    </div>
+    <div class="w3-third w3-display-container">
+      <img class="slideshow2" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+      <img class="slideshow2" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+      <img class="slideshow2" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+      <img class="slideshow2" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+      <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+      <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
+    </div>
 </div>
 
 <script>
