da001d405951a6fc006cce31c39cfe96f9194a10
Philipp Gross <pka.gross@web.de>
2021-01-03 00:46:32 +0100

    Zweite Slideshow hinzugefügt

--- public/liga/jubilaeum.php
+++ public/liga/jubilaeum.php
@@ -18,27 +18,39 @@ include '../../templates/header.tmp.php';
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
 
-    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 0)">&#10094;</button>
+    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 0)">&#10095;</button>
+</div>
+
+<div class="w3-content w3-display-container">
+    <img class="slideshow2" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+    <img class="slideshow2" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+    <img class="slideshow2" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+    <img class="slideshow2" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+
+    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
 </div>
 
 <script>
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = [1,1];
+var slideId = ["slideshow1","slideshow2"];
+showDivs(1,0);
+showDivs(1,1);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
 }
 
-function showDivs(n) {
+function showDivs(n, no) {
   var i;
-  var x = document.getElementsByClassName("slideshow1");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex[no]-1].style.display = "block";  
 }
 </script>
 
