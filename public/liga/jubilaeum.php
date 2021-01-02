a9ff90bd71b152717fb7b484660fc6d963136f49
Philipp Gross <pka.gross@web.de>
2021-01-03 00:36:43 +0100

    Erste Slideshow hinzugefügt

--- public/liga/jubilaeum.php
+++ public/liga/jubilaeum.php
@@ -12,11 +12,35 @@ $content = "Spielerprofile für das 25. Jubiläum der Deutschen Einradhockeyliga
 include '../../templates/header.tmp.php';
 ?>
 
-<div class="w3-content">
+<div class="w3-content w3-display-container">
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
     <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+
+    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
 </div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideshow1");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+
 
 <?php include '../../templates/footer.tmp.php';
