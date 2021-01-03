69b7589b8038d35cd23990a9b08c43a963f0f13f
Philipp Gross <pka.gross@web.de>
2021-01-03 13:04:35 +0100

    Layout verändert

--- public/liga/jubilaeum.php
+++ public/liga/jubilaeum.php
@@ -12,36 +12,52 @@ $content = "Spielerprofile für das 25. Jubiläum der Deutschen Einradhockeyliga
 include '../../templates/header.tmp.php';
 ?>
 
-<div class="w3-row">
+<div class="w3-container w3-card-4 w3-primary w3-margin w3-round">
   <div class="w3-third w3-padding">
-    <div class="w3-display-container">
-      <img class="slideshow1" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
-      <img class="slideshow1" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
-      <img class="slideshow1" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
-      <img class="slideshow1" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+    <div class="w3-display-container w3-round w3-card" style="border: 1px solid white;">
+      <img class="slideshow1 w3-round" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+      <img class="slideshow1 w3-round" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+      <img class="slideshow1 w3-round" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+      <img class="slideshow1 w3-round" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
       <button class="w3-button w3-light-grey w3-display-left w3-opacity" onclick="plusDivs(-1, 0)">&#10094;</button>
       <button class="w3-button w3-light-grey w3-display-right w3-opacity" onclick="plusDivs(1, 0)">&#10095;</button>
     </div>
   </div>
+  <div class="w3-twothird">
+    <p class="w3c-margin">Das ist ein Text.</p>
+  </div>
+</div>
+
+
+<div class="w3-container w3-card-4 w3-primary w3-margin w3-round">
+  <div class="w3-twothird w3-right-align">
+    <p>Das ist ein Text.</p>
+  </div>
   <div class="w3-third w3-padding">
-    <div class="w3-display-container">
-      <img class="slideshow2" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
-      <img class="slideshow2" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
-      <img class="slideshow2" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
-      <img class="slideshow2" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+    <div class="w3-display-container w3-card w3-round" style="border: 1px solid white;">
+      <img class="slideshow2 w3-round" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+      <img class="slideshow2 w3-round" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+      <img class="slideshow2 w3-round" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+      <img class="slideshow2 w3-round" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
       <button class="w3-button w3-light-grey w3-display-left w3-opacity" onclick="plusDivs(-1, 1)">&#10094;</button>
       <button class="w3-button w3-light-grey w3-display-right w3-opacity" onclick="plusDivs(1, 1)">&#10095;</button>
     </div>
   </div>
+</div>
+
+<div class="w3-container w3-card-4 w3-primary w3-margin w3-round">
   <div class="w3-third w3-padding">
-    <div class="w3-display-container">
-      <img class="slideshow3" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
-      <img class="slideshow3" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
-      <img class="slideshow3" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
-      <img class="slideshow3" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
+    <div class="w3-display-container w3-card w3-round" style="border: 1px solid white;">
+      <img class="slideshow3 w3-round" src="../bilder/spielerprofile/Adrian.jpg" style="width:100%">
+      <img class="slideshow3 w3-round" src="../bilder/spielerprofile/Adrian2.jpg" style="width:100%">
+      <img class="slideshow3 w3-round" src="../bilder/spielerprofile/Adrian6.jpg" style="width:100%">
+      <img class="slideshow3 w3-round" src="../bilder/spielerprofile/Adrian7.jpg" style="width:100%">
       <button class="w3-button w3-light-grey w3-display-left w3-opacity" onclick="plusDivs(-1, 2)">&#10094;</button>
       <button class="w3-button w3-light-grey w3-display-right w3-opacity" onclick="plusDivs(1, 2)">&#10095;</button>
     </div>
+  </div>
+  <div class="w3-twothird">
+    <p>Das ist ein Text.</p>
   </div>
 </div>
 
