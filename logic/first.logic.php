48fd91a87f1f01dd34c40cc1a9433d70ed522440
Ansgar <merkmich@gmail.com>
2021-03-07 14:25:48 +0100

    ini_set in first.logic eingebaut

--- logic/first.logic.php
+++ logic/first.logic.php
@@ -1,17 +1,47 @@
 <?php
 
 // Dieses Dokument muss immer als Erstes für die Logik geladen werden
+// Die Reihenfolge, wann was geladen wird, ist wichtig
 
 /**
- * php.ini Einstellungen
+ * PHP-Einstellungen vornehmen
  */
-require_once __DIR__ . '/../system/ini_set.php';
+// Session-Sicherheit
+ini_set('session.cookie_lifetime', '1200');
+ini_set('session.use_cookies', '1');
+ini_set('session.use_only_cookies', '1');
+ini_set('session.use_strict_mode', '1');
+ini_set('session.cookie_httponly', '1');
+ini_set('session.cookie_samesite ', '"Strict"');
+ini_set('session.cookie_secure', '1');
+// https://www.php.net/manual/de/session.security.ini.php
+// https://www.php.net/manual/de/features.session.security.management.php
+
+// Sonstige Einstellungen
+ini_set('date.timezone', 'Europe/Berlin');
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '20M');
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', '90');
+ini_set('error_reporting', E_ALL);
+ini_set('log_errors', 'On');
+ini_set('display_errors', 'Off');
+ini_set('error_log', __DIR__ . '/logs/errors.log');
+
+// Nur für Localhost-Einstellungen
+if (
+    file_exists(__DIR__ . '/../nur_localhost_nicht_hochladen.php')
+    && in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])
+) {
+    require_once __DIR__ . '/../nur_localhost_nicht_hochladen.php';
+}
 
 
 /**
  * Enviroment-Variablen laden
  */
 require_once __DIR__ . '/../env.php';
+
 
 /**
  * Security-Header
@@ -22,30 +52,26 @@ header('X-Content-Type-Options: nosniff');
 header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
 header('Referrer-Policy: no-referrer-when-downgrade');
 
-/**
- * Session starten
- */
-session_start();
 
 /**
+ * Session starten
  * Session-Hijacking erschweren
  * https://owasp.org/www-community/attacks/Session_fixation
  * https://www.php.net/session_regenerate_id
  *
  * Bei mangelnder Performance session_regenerate_id nur bei Login und Logout einrichten.
  */
+session_start();
 if (
     isset($_SESSION['destroyed'])
     && $_SESSION['destroyed'] < time() - 15
 ) {
     unset ($_SESSION['logins']);
-    trigger_error("Zugriff auf ungültige Session-ID.", E_USER_ERROR);
+    trigger_error("Ungültige Session-ID.", E_USER_ERROR);
 }
 
 $_SESSION['destroyed'] = time(); // Legt den Destroy-Zeitstempel fest
-
 session_regenerate_id(); // Kopiert die bestehende Session
-
 unset($_SESSION['destroyed']); // Die neue Session braucht keinen Destroy-Zeitstempel
 
 /**
@@ -65,9 +91,10 @@ spl_autoload_register(
  * Wartungsmodus
  */
 if ((Env::WARTUNGSMODUS) && !isset($_SESSION['wartungsmodus'])) {
-    $_SESSION['error']['text'] = 'Die Seite befindet sich im Wartungsmodus';
+    $_SESSION['error']['text'] = 'Die Seite befindet sich im Wartungsmodus. Bitte probiert es später wieder.';
     Helper::reload('/errors/500.php');
 }
+
 
 /**
  * Diese Funktion wird nach Beendigung des Skriptes ausgeführt.
@@ -99,8 +126,8 @@ register_shutdown_function(static function () {
             $_SESSION['error']['url'] = $_SERVER['REQUEST_URI'];
             Helper::reload("/errors/500.php");
         }
-
     }
+
     // Logs der Besucher
 
     // Referrer falls relevant
@@ -128,3 +155,9 @@ register_shutdown_function(static function () {
  * Verbindung zur Datenbank
  */
 dbi::initialize(); // Neue DB-Verbindung mit Prepared-Statements
+
+
+/**
+ * Sprache für Zeitformate in Deutsch --> strftime()
+ */
+setlocale(LC_TIME, 'de_DE@euro', 'de_DE', 'de', 'ge');
