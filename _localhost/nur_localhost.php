a91f3e06710898dd27a4e288f6323701c394e6a4
Ansgar <merkmich@gmail.com>
2021-03-07 23:47:10 +0100

    Update nur_localhost.php

--- _localhost/nur_localhost.php
+++ _localhost/nur_localhost.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Diese Datei soll nicht auf dem Server liegen.
- * Sie überschreibt PHP-Einstellungen in system/ini_set.php
+ * Sie überschreibt PHP-Einstellungen in init.php
  */
 ini_set('session.cookie_secure', '0'); // $_SESSION Funktioniert auch ohne https
 ini_set('display_errors', 'On'); // Fehler werden angzeigt und nicht nur geloggt
