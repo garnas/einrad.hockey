f002914c0a8aa7b9ff98e092432dd20251639a34
Ansgar <merkmich@gmail.com>
2021-07-03 17:57:31 +0200

    Saisonbeginn angepasst

--- classes/config.class.php
+++ classes/config.class.php
@@ -6,7 +6,7 @@ class Config
      * Saison
      */
     public const SAISON = 27; // Saison 0 = Jahr 1995;
-    public const SAISON_ANFANG = '14.08.2021';
+    public const SAISON_ANFANG = '16.08.2021';
     public const SAISON_ENDE = '29.05.2022';
     /**
      * Log-Files
