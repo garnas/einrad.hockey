d0d263f381b2da4d4201bbf9f954a27bae713684
Ansgar <merkmich@gmail.com>
2021-08-16 16:12:36 +0200

    Localhost Konstante hinzugefügt

--- _localhost/nur_localhost.php
+++ _localhost/nur_localhost.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Diese Datei soll nicht auf dem Server liegen.
- * Sie überschreibt PHP-Einstellungen in init.php
+ * Diese Datei überschreibt PHP-Einstellungen in init.php für den Localhost, wenn Env::IS_LOCALHOST true ist.
  */
 ini_set('session.cookie_secure', '0'); // $_SESSION Funktioniert auch ohne https
 ini_set('display_errors', 'On'); // Fehler werden angzeigt und nicht nur geloggt
