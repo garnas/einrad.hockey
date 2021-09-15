9c24e677278dbc1847f2eb3fef1e300781da2a09
Largofin <pka.gross@web.de>
2021-09-15 20:44:08 +0200

    Datenbank aktualisiert

--- _localhost/db_anonymisieren.sql
+++ _localhost/db_anonymisieren.sql
@@ -17,7 +17,7 @@ UPDATE spieler SET vorname= 'Vorname',nachname='Nachname',jahrgang= 2020, geschl
 
 -- Ligaleitung
 UPDATE ligaleitung SET passwort = NULL, email = 'test@einrad.hockey';
-INSERT INTO spieler (spieler_id, team_id, vorname, nachname, jahrgang, geschlecht, schiri, junior, letzte_saison, zeit) VALUES ('3', NULL, 'Vorname', 'Entwickler', '1234', 'd', '', 'Nein', '26', current_timestamp());
+INSERT INTO spieler (spieler_id, team_id, vorname, nachname, jahrgang, geschlecht, schiri, junior, letzte_saison, timestamp) VALUES ('3', NULL, 'Vorname', 'Entwickler', '1234', 'd', '', 'Nein', '26', current_timestamp());
 INSERT INTO ligaleitung(funktion, login, passwort, spieler_id, email) VALUES ('ligaausschuss', 'entwickler', '$2y$10$ErHYjOnzowhPpBAqWRjOG.YsaaBJiR/IybP.1pdyWt4MBPlKNpN5e', '3', 'test@einrad.hockey');
 
 -- Turniere
