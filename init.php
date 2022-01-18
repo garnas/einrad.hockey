dbeb3915bf6a8f53a3a273f0383c3f4d9edd7302
Largofin <pka.gross@web.de>
2022-01-18 11:57:50 +0100

    Hotfix Autoload

--- init.php
+++ init.php
@@ -59,7 +59,6 @@ header('Referrer-Policy: no-referrer-when-downgrade');
  */
 spl_autoload_register(
     static function ($class) {
-        $class = strtolower($class);
         include Env::BASE_PATH . '/classes/' . $class . '.class.php';
     }
 );
