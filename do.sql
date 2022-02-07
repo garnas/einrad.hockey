2c1b548c98b84f1808c6c5ff799fdc9881acbd8b
ansgar.poelking <merkmich@gmail.com>
2022-02-07 20:05:43 +0100

    .sql Dateien aufgeräumt

--- do.sql
+++
@@ -1,9 +0,0 @@
-UPDATE `spieler` SET `schiri`='27' WHERE schiri = 'Ausbilder/in';
-ALTER TABLE `spieler` CHANGE `schiri` `schiri` INT(6) NULL DEFAULT NULL;
-UPDATE `spieler` SET `schiri`= NULL WHERE schiri = 0;
-UPDATE `spieler` SET `schiri`= 27 WHERE schiri = 26;
-ALTER TABLE `spieler` CHANGE `zeit` `timestamp` TIMESTAMP NULL;
-ALTER TABLE `spieler` CHANGE `junior` `junior` ENUM('Ja','Nein','') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'Nein';
-UPDATE `spieler` SET `junior`= NULL WHERE junior != 'Ja';
-ALTER TABLE `spieler` CHANGE `junior` `junior` ENUM('Ja') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
-
