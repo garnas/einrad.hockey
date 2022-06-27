f5069bf52fbadfbc52d1350ba4e1695201f4f1b7
ansgar.poelking <merkmich@gmail.com>
2022-06-28 01:15:49 +0200

    Update archiv.php

--- public/liga/archiv.php
+++ public/liga/archiv.php
@@ -16,7 +16,7 @@ include '../../templates/header.tmp.php';
     <h1 class="w3-text-primary">Archiv</h1>
     <p>---</p>
     <p><?= Html::link('ergebnisse.php?saison=27', 'Turniere der Saison 2021/22') ?></p>
-    <p><?= Html::link('ergebnisse.php?saison=27', 'Turniere der Saison 2021/22') ?></p>
+    <p><?= HTML::link('tabelle.php?saison=27', 'Tabelle der Saison 2021/22') ?></p>
     <p>---</p>
     <p><?= HTML::link('tabelle.php?saison=26', 'Tabelle der Saison 2020/21') ?></p>
     <p><?= Html::link('ergebnisse.php?saison=26', 'Turniere der Saison 2020/21') ?></p>
