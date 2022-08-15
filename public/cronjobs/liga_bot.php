ce979c5c2cc83a3551d2f7582f42ef975b8b76a5
ansgar.poelking <merkmich@gmail.com>
2022-08-15 18:01:42 +0200

    Update liga_bot.php

--- public/cronjobs/liga_bot.php
+++ public/cronjobs/liga_bot.php
@@ -6,16 +6,7 @@ require_once '../../init.php';
 
 $_SESSION['logins']['cronjob'] = 'Cronjob';
 
-LigaBot::liga_bot(); //TODO Helper::log verwenden
+//LigaBot::liga_bot(); //TODO Helper::log verwenden
 
-////Meldungen protokollieren
-//foreach (($_SESSION['e_messages'] ?? array()) as $message){
-//    echo 'Fehler: ' . $message . '<br><br>';
-//}
-//foreach (($_SESSION['w_messages'] ?? array()) as $message){
-//    echo 'Hinweis: ' . $message . '<br><br>';
-//}
-//foreach (($_SESSION['a_messages'] ?? array()) as $message){
-//    echo 'Info: ' . $message . '<br><br>';
-//}
+
 session_destroy();
