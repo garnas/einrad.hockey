7d7c18365a94734b70b841714eee72db1066d24e
ansgar.poelking <merkmich@gmail.com>
2022-12-11 10:54:52 +0100

    Create lc_checkmail.php

---
+++ public/ligacenter/lc_checkmail.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../../init.php';
+require_once '../../logic/session_la.logic.php'; //Auth
+
+
+include '../../templates/header.tmp.php'; ?>
+
+    <h1>Alle Mailbot-Mails der letzten 30 Tage</h1>
+    <h3 class="w3-bottombar"></h3>
+<?php
+$mails = \App\Repository\DoctrineWrapper::manager()
+    ->getRepository(\App\Entity\Sonstiges\Mailbot::class)->findAll();
+
+foreach ($mails as $mail) {
+    if($mail->getZeit()->getTimestamp() > time() - 30 * 24 * 60 * 60){ ?>
+        <p class="w3-center"><b>An</b></p>
+        <p class="w3-center"><?=$mail->getAdressat()?></p>
+        <p class="w3-center"><b>Betreff</b></p>
+        <p class="w3-center"><?=$mail->getBetreff()?></p>
+        <p><b>Inhalt</b></p>
+        <p><?=$mail->getInhalt()?></p>
+        <h3 class="w3-bottombar"></h3>
+    <?php }
+}
+include '../../templates/footer.tmp.php';
