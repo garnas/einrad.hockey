5b3c92303717d1b932219a8e7f677668ffc7d937
Largofin <pka.gross@web.de>
2023-04-05 12:49:44 +0200

    extended tables

--- templates/teamstats/angstgegner.tmp.php
+++ templates/teamstats/angstgegner.tmp.php
@@ -12,6 +12,7 @@
                 <th>Team</th>
                 <th class="w3-center">Sieg / Unentschieden / Niederlage</th>
                 <th class="w3-center">Tore:Gegentore</th>
+                <th class="w3-center">Differenz</th>
             </tr>
             </thead>
             <?php foreach ($angst as $team_id => $team): ?>
@@ -19,6 +20,7 @@
                     <td><?=TeamRepository::get()->team($team_id)->getName();?></td>
                     <td class="w3-center"><?=$team['win']?> / <?=$team['draw']?> / <?=$team['loss']?></td>
                     <td class="w3-center"><?=$team['goals']?>:<?=$team['goals_against']?></td>
+                    <td class="w3-right"><?=$team['goals']-$team['goals_against']?></td>
                 </tr>
             <?php endforeach; ?>
         </table>
