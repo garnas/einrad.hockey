74a236f60669cc8b7513014725869e938344b6e3
ansgar.poelking <merkmich@gmail.com>
2023-06-16 23:35:27 +0200

    Create a_final.php

---
+++ public/liga/a_final.php
@@ -0,0 +1,25 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LOGIK////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+require_once '../../init.php';
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LAYOUT///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+Html::$titel = 'Spielplan B-Meisterschaft';
+Html::$content = 'Aktuelle Ergebnisse der B-Meisterschaft';
+include '../../templates/header.tmp.php';
+?>
+
+    <!-- Archiv -->
+    <h1 class="w3-text-primary">Spielplan Deutsche Meisterschaft</h1>
+<p>
+    <?= HTML::link("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWCCtPxRlVZ6EAh6TSxoAGa0Lc0alfo2iqjWBz0rmGoM1deUl5hYPMORPAnn9gCq67eysaWUjGlqSK/pubhtml",
+    bezeichnung: "Direkter Link zum Spielplan",
+    extern: true,
+    icon: "launch") ?>
+</p>
+    <iframe style="width:100%;height:800px" class="archiv w3-border-0"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWCCtPxRlVZ6EAh6TSxoAGa0Lc0alfo2iqjWBz0rmGoM1deUl5hYPMORPAnn9gCq67eysaWUjGlqSK/pubhtml?gid=460300906&amp;single=true&amp;widget=false&amp;headers=false"></iframe>
+<?php include '../../templates/footer.tmp.php';
