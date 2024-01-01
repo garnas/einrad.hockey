9b0aaf0091ab80c8d47357d8709a825065408216
ansgar.poelking <merkmich@gmail.com>
2024-01-01 16:24:55 +0100

    Nav Icon entfernt

--- classes/nav.class.php
+++ classes/nav.class.php
@@ -70,7 +70,7 @@ class Nav
         $class_text_color = (isset($_SESSION['logins']['team'])) ? "" : "w3-text-grey";
         $links = [
             [Env::BASE_URL . "/teamcenter/tc_start.php", "Start", $class_text_color],
-            [Env::BASE_URL . "/teamcenter/tc_terminseite_erstellen.php", Html::icon("fiber_new") . " Teamtermine", $class_text_color],
+            [Env::BASE_URL . "/teamcenter/tc_terminseite_erstellen.php", "Teamtermine", $class_text_color],
             [Env::BASE_URL . "/teamcenter/tc_turnierliste_anmelden.php", "Turnieranmeldung", $class_text_color],
             [Env::BASE_URL . "/teamcenter/tc_turnier_erstellen.php", "Turnier erstellen", $class_text_color],
             [Env::BASE_URL . "/teamcenter/tc_turnierliste_verwalten.php", "Eigene Turniere", $class_text_color],
