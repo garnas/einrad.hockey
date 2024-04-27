53302ab0cdf0e55f51c56bb241719a1fbe0f8a18
ansgar.poelking <merkmich@gmail.com>
2024-04-27 12:46:16 +0200

    Create spielplan_teams_tauschen.sql

---
+++ _localhost/spielplan_teams_tauschen.sql
@@ -0,0 +1,25 @@
+UPDATE `spiele` SET `team_id_a`= 1054 WHERE turnier_id = 1181 and team_id_a = 310;
+UPDATE `spiele` SET `team_id_b`= 1054 WHERE turnier_id = 1181 and team_id_b = 310;
+UPDATE `spiele` SET `schiri_team_id_a`= 1054 WHERE turnier_id = 1181 and schiri_team_id_a = 310;
+UPDATE `spiele` SET `schiri_team_id_b`= 1054 WHERE turnier_id = 1181 and schiri_team_id_b = 310;
+
+UPDATE `spiele` SET `team_id_a`= 1042 WHERE turnier_id = 1181 and team_id_a = 905;
+UPDATE `spiele` SET `team_id_b`= 1042 WHERE turnier_id = 1181 and team_id_b = 905;
+UPDATE `spiele` SET `schiri_team_id_a`= 1042 WHERE turnier_id = 1181 and schiri_team_id_a = 905;
+UPDATE `spiele` SET `schiri_team_id_b`= 1042 WHERE turnier_id = 1181 and schiri_team_id_b = 905;
+
+UPDATE `spiele` SET `team_id_a`= 905 WHERE turnier_id = 1181 and team_id_a = 1054;
+UPDATE `spiele` SET `team_id_b`= 905 WHERE turnier_id = 1181 and team_id_b = 1054;
+UPDATE `spiele` SET `schiri_team_id_a`= 905 WHERE turnier_id = 1181 and schiri_team_id_a = 1054;
+UPDATE `spiele` SET `schiri_team_id_b`= 905 WHERE turnier_id = 1181 and schiri_team_id_b = 1054;
+
+UPDATE `spiele` SET `team_id_a`= 310 WHERE turnier_id = 1181 and team_id_a = 1042;
+UPDATE `spiele` SET `team_id_b`= 310 WHERE turnier_id = 1181 and team_id_b = 1042;
+UPDATE `spiele` SET `schiri_team_id_a`= 310 WHERE turnier_id = 1181 and schiri_team_id_a = 1042;
+UPDATE `spiele` SET `schiri_team_id_b`= 310 WHERE turnier_id = 1181 and schiri_team_id_b = 1042;
+
+
+UPDATE `spiele` SET `team_id_a`= 1054 WHERE turnier_id = 1181 and team_id_a = 310;
+UPDATE `spiele` SET `team_id_b`= 1054 WHERE turnier_id = 1181 and team_id_b = 310;
+UPDATE `spiele` SET `schiri_team_id_a`= 1054 WHERE turnier_id = 1181 and schiri_team_id_a = 310;
+UPDATE `spiele` SET `schiri_team_id_b`= 1054 WHERE turnier_id = 1181 and schiri_team_id_b = 310;
