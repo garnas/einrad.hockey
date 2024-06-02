11a8ba5c81db199e38e7959fe73ff5fc5f3eada9
ansgar.poelking <merkmich@gmail.com>
2024-06-02 20:12:52 +0200

    Issue 133 Fix Anzahl Ligaturniere

--- classes/nturnier.class.php
+++ classes/nturnier.class.php
@@ -516,6 +516,7 @@ class nTurnier
                 ON teams_liga.team_id = turniere_liga.ausrichter
                 WHERE phase = 'ergebnis'
                 AND saison = ?
+                AND (art = 'I' OR art = 'II' OR art = 'III')
                 ORDER BY turniere_liga.datum " . ($asc ? "asc" : "desc");
         return db::$db->query($sql, $saison)->fetch_objects(__CLASS__, key: 'turnier_id');
     }
