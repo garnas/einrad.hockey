57ad40164838a3aad4c2c317ba499fd179630491
Philipp Gross <pka.gross@web.de>
2024-12-18 15:18:58 +0100

    reworked the grid navigation for center

--- public/ligacenter/lc_start.php
+++ public/ligacenter/lc_start.php
@@ -5,49 +5,29 @@
 require_once '../../init.php';
 require_once '../../logic/session_la.logic.php'; //auth
 
-$centerpanels = Nav::get_lc_start();
+$panels = Nav::get_lc_start();
+$downloads = Nav::get_lc_downloads();
 
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
-Html::$page_width = "660px";
 include '../../templates/header.tmp.php'; ?>
 
-
 <h1 class='w3-center w3-text-primary'>Hallo <?= LigaLeitung::get_details($_SESSION['logins']['la']['login'])['vorname'] ?>!</h1>
-<?= Html::link(Env::BASE_URL . "/ligacenter/lc_turnierstats.php", "turniere.xlsx", extern: true) ?>
-<br>
-<?= Html::link(Env::BASE_URL . "/ligacenter/lc_spielerstats.php","spieler.xlsx", extern: true) ?>
-<br>
-<?= Html::link(Env::BASE_URL . "/ligacenter/lc_teamstats.php", "teams.xlsx", extern: true) ?>
-<br>
-<?= Html::link(Env::BASE_URL . "/schiricenter/schiritest_stats.php", "schiritest.xlsx", extern: true) ?>
-<br>
+<div class="flex-container">
+    <?php foreach ($panels as $panel): ?>
+        <a class="no flex-item <?= $panel[2] ?> w3-round-xlarge w3-hover-opacity" href="<?= $panel[0] ?>">
+            <?= $panel[1] ?>
+        </a>
+    <?php endforeach; ?>
+</div>
 
-<div id="messen" class="">
-    <!-- Misst die Fensterbreite des Browsers, um die Centerpanels gleichmäßig verteilen zu können -->
-    <div id="apps" class="w3-content">
-        <div class="w3-row">
-
-            <?php foreach ($centerpanels as $centerpanel) { ?>
-                <a class="" href="<?= $centerpanel[0] ?>">
-                    <div class="w3-col <?= $centerpanel[2] ?> w3-round-xxlarge w3-hover-opacity w3-display-container centerpanels">
-                        <div class="w3-display-middle w3-large w3-center">
-                            <?= $centerpanel[1] ?>
-                        </div>
-                    </div>
-                </a>
-            <?php } //end foreach?>
-
-        </div> <!-- w3-row -->
-    </div> <!-- w3-apps -->
-</div> <!-- w3-messen -->
-
-<script>
-    //setzt die Margin der centerpanels-Quadrate so, dass sie immer gleich verteilt sind
-    window.addEventListener("resize", centerpanels_anordnung);
-    centerpanels_anordnung();
-</script>
+<div class="flex-container w3-border-top w3-border-grey">
+    <?php foreach ($downloads as $download): ?>
+        <a class="no flex-item <?= $download[2] ?> w3-round-xlarge w3-hover-opacity" href="<?= $download[0] ?>">
+            <?= $download[1] ?>
+        </a>
+    <?php endforeach; ?>
+</div>
 
 <?php include '../../templates/footer.tmp.php'; ?>
-
--- public/teamcenter/tc_start.php
+++ public/teamcenter/tc_start.php
@@ -5,40 +5,22 @@
 require_once '../../init.php';
 require_once '../../logic/session_team.logic.php'; //auth
 
-$centerpanels = Nav::get_tc_start();
+$panels = Nav::get_tc_start();
+
 Html::info("In euren " . Html::link(Env::BASE_URL . "/teamcenter/tc_teamdaten.php", "Teamdaten")
-    . " könnt ihr mögliche Bonus-Freilose für frühzeitig gesetzte Freilose und angemeldete Turniere sehen.", esc:false);
+    . " könnt ihr mögliche Bonus-Freilose für frühzeitig gesetzte Freilose und angemeldete Turniere sehen.", esc: false);
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
-Html::$page_width = "660px";
 include '../../templates/header.tmp.php'; ?>
 
 <h1 class='w3-center w3-text-primary'>Hallo <?= $_SESSION['logins']['team']['name'] ?>!</h1>
-<div id="messen" class="">
-    <!-- Misst die Fensterbreite des Browsers, um die Centerpanels gleichmäßig verteilen zu können -->
-    <div id="apps" class="w3-content">
-        <div class="w3-row">
-
-            <?php foreach ($centerpanels as $centerpanel) { ?>
-                <a class="" href="<?= $centerpanel[0] ?>">
-                    <div class="w3-col <?= $centerpanel[2] ?> w3-round-xxlarge w3-hover-opacity w3-display-container centerpanels">
-                        <div class="w3-display-middle w3-large w3-center">
-                            <?= $centerpanel[1] ?>
-                        </div>
-                    </div>
-                </a>
-            <?php } //end foreach?>
-
-        </div> <!-- w3-row -->
-    </div> <!-- apps -->
-</div> <!-- messen -->
-
-<script>
-    //setzt die Margin der centerpanels-Quadrate so, dass sie immer gleich verteilt sind
-    window.addEventListener("resize", centerpanels_anordnung);
-    centerpanels_anordnung();
-</script>
+<div class="flex-container">
+    <?php foreach ($panels as $panel): ?>
+        <a class="no flex-item <?= $panel[2] ?> w3-round-xlarge w3-hover-opacity" href="<?= $panel[0] ?>">
+            <?= $panel[1] ?>
+        </a>
+    <?php endforeach; ?>
+</div>
 
 <?php include '../../templates/footer.tmp.php'; ?>
-
