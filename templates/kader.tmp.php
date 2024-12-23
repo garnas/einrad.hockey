c8fe7b3f843a02ce101c4b4e5388e790c72d4fbb
Dominik <Bilitewski.D@gmx.de>
2024-12-23 17:49:12 +0100

    hallo

--- templates/kader.tmp.php
+++ templates/kader.tmp.php
@@ -179,7 +179,7 @@
     // Get the modal
     var modal = document.getElementById('spieler_eintragen');
 
-    // When the user clicks anywhere outside of the modal, close it
+    // When the user clicks anywhere outside of the modal, close it Hallo Welt
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
