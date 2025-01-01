a4fc7bc003be535ee2a9ea35c6d3622a44b64ea1
Ansgar Pölking <merkmich@gmail.com>
2025-01-01 21:41:45 +0100

    Mail TMP added

---
+++ templates/mails/mail_freilos_recycle.tmp.php
@@ -0,0 +1,5 @@
+<p>Hallo <?= $team->getName() ?>, </p>
+<p>
+    da ihr euer Freilos acht Wochen vor Turnierbeginn gesetzt habt, wurde euch ein neues Freilos gutgeschrieben.
+    Eine Übersicht über deine Freilose findest du <a href='<?= Env::BASE_URL ?>/teamcenter/tc_teamdaten.php'>hier</a>.
+</p>
