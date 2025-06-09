b569d135718a3e848011a4f5102ce5e4ebb1864f
Philipp Gross <philipp@einrad.hockey>
2025-06-09 21:44:57 +0200

    added predicition game to the nav

--- classes/nav.class.php
+++ classes/nav.class.php
@@ -105,6 +105,7 @@ class Nav
         return [
             [Env::BASE_URL . "/liga/ueber_uns.php", "Über uns"],
             [Env::BASE_URL . "/liga/jubilaeum.php", "25 Jahre Liga"],
+            [Env::BASE_URL . "/liga/tippspiel.php", "Tippspiel"],
             [Env::BASE_URL . "/liga/archiv.php", "Archiv"],
             [Env::BASE_URL . "/login.php", "Ausschusslogin"],
             [Env::BASE_URL . "/liga/kontakt.php", "Kontakt"],
