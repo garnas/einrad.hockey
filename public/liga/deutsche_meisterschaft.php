dd547a50e38009db02df5f8212fc8d8dec05703c
Philipp Gross <philipp@einrad.hockey>
2025-06-19 01:29:29 +0200

    added links and schedule for the livestream

--- public/liga/deutsche_meisterschaft.php
+++ public/liga/deutsche_meisterschaft.php
@@ -12,30 +12,48 @@ Html::$content = 'Spielplan Deutsche Meisterschaft';
 include '../../templates/header.tmp.php';
 ?>
 
-<h1 class="w3-text-primary">Deutsche Meisterschaft 2025 in Dresden</h1>
+<h1 class="w3-text-primary">Deutsche Meisterschaft 2025</h1>
 
-<h3 class="w3-text-secondary">Spielplan</h3>
-<iframe 
-    style="width:100%; height:800px"
-    class="w3-border-0"
-    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ712b8anQI3ZDXPVFdFEomKgA3zyyNBLT4pT28iJXasOgxWLFeqWNRnVUjQxDlLQ/pubhtml?gid=62217435&single=true">
-</iframe>
-
+<h3 class="w3-text-secondary">Spielplan zur Deutschen Meisterschaft 2025</h3>
 <p>
     <?= HTML::link("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ712b8anQI3ZDXPVFdFEomKgA3zyyNBLT4pT28iJXasOgxWLFeqWNRnVUjQxDlLQ/pubhtml?gid=62217435&single=true",
     bezeichnung: "Direkter Link zum Spielplan",
     extern: true,
     icon: "launch") ?>    
 </p>
-<p class="w3-text-grey w3-small">
-    Hinweis: Um neue Ergebnisse einsehen zu können, muss der Spielplan aktualisiert werden. Dafür bitte die entsprechende Seite neu laden.
-</p>
 
-<h3 class="w3-text-secondary">Livestream</h3>
-<p>Für die Deutsche Meisterschaft wird es einen Livestream geben. Der Link wird dann hier veröffentlicht.</p>
-<!-- <p>
-    <iframe src="https://player.twitch.tv/?channel=holzmichel41&parent=einrad.hockey" frameborder="0" allowfullscreen="true" scrolling="no"
-            width="100%"></iframe>
-</p> -->
+<iframe 
+    style="width:100%; height:800px"
+    class="w3-border-0"
+    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ712b8anQI3ZDXPVFdFEomKgA3zyyNBLT4pT28iJXasOgxWLFeqWNRnVUjQxDlLQ/pubhtml?gid=62217435&single=true">
+</iframe>
+
+<h3 class="w3-text-secondary">Livestream zur Deutschen Meisterschaft 2025</h3>
+
+<h4>Samstag, 21. Juni 2025</h4>
+<div class="w3-leftbar" style="margin-bottom: 15px;">
+    <ul class="w3-ul">
+        <li><?=HTML::icon("schedule", class:"w3-text-primary")?> ab ca. 10:00 Uhr</li>
+        <li><?=HTML::icon("videocam", class:"w3-text-primary")?> Live auf Twitch</li>
+        <li><?=HTML::icon("launch", class:"w3-text-primary")?> <?=HTML::link("https://www.twitch.tv/einradhockeytv", bezeichnung: "Zum Livestream auf Twitch", extern: true, icon: "") ?></li>
+    </ul>
+</div>
+
+<h4>Sonntag, 22. Juni 2025</h4>
+<div class="w3-leftbar" style="margin-bottom: 15px;">
+    <ul class="w3-ul">
+        <li><?=HTML::icon("schedule", class:"w3-text-primary")?> ab ca. 09:00 Uhr</li>
+        <li><?=HTML::icon("videocam", class:"w3-text-primary")?> Weiterhin live auf Twitch</li>
+        <li><?=HTML::icon("launch", class:"w3-text-primary")?> <?=HTML::link("https://www.twitch.tv/einradhockeytv", bezeichnung: "Zum Livestream auf Twitch", extern: true, icon: "") ?></li>
+    </ul>
+</div>
+
+<div class="w3-leftbar" style="margin-bottom: 15px;">
+    <ul class="w3-ul">
+        <li><?=HTML::icon("schedule", class:"w3-text-primary")?> ab ca. 13:00 Uhr</li>
+        <li><?=HTML::icon("videocam", class:"w3-text-primary")?> Wechsel zur Übertragung im MDR</li>
+        <li><?=HTML::icon("launch", class:"w3-text-primary")?> <?=HTML::link("https://www.mdr.de/tv/livestreams/event-sendungen/sendung-1036754.html", bezeichnung: "Zum Livestream beim MDR", extern: true, icon: "") ?></li>
+    </ul>
+</div>
 
 <?php include '../../templates/footer.tmp.php';
