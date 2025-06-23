736da4221dcd634e0d4929e49a5d197224ffc405
Ansgar <a.poelking@gmai.com>
2025-06-23 23:45:14 +0200

    Kleine Fixes fürs Tippspiel

--- public/liga/tippspiel.php
+++ public/liga/tippspiel.php
@@ -10,11 +10,11 @@ include '../../templates/header.tmp.php';
 
 <h1 class="w3-text-primary">Tippspiel für die Meisterschaften der Saison 2024/25</h1>
 
-<h3 class="w3-text-secondary">Ergebnisse Deutsche Meisterschaft 2024/25</h1>
+<h3 class="w3-text-secondary">Ergebnisse Deutsche Meisterschaft 2024/25</h3>
 <table class="w3-border w3-border-light-grey w3-table w3-striped">
     <tr class="w3-primary"><th>Platz</th><th>Name</th><th>Team</th><th>Punkte</th><th>Details</th></tr>
     <tr><td>1</td><td>Lucas</td><td>SKV Mörfelden Phönix</td><td>39</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_PHX_Lucas.pdf'>PDF</a></td></tr>
-    <tr><td>2</td><td>Oliver</td><td>SKV Mörfelden Phönix</td><td>36</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_PHX_Oliver.pdf'>PDF</a></td></tr>
+    <tr><td>2</td><td>Oliver</td><td>SKV Mörfelden Phönix</td><td>36</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_PHX_Oli.pdf'>PDF</a></td></tr>
     <tr><td>3</td><td>Hannah</td><td>TV Lilienthal Moorteufel</td><td>35</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_MRT_Hannah.pdf'>PDF</a></td></tr>
     <tr><td>4</td><td>Janina</td><td>TV Lilienthal Moorteufel</td><td>30</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_MRT_Janina.pdf'>PDF</a></td></tr>
     <tr><td>5</td><td>Kristin</td><td>MUH-Bande Jena</td><td>28</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_MUH_Kristin.pdf'>PDF</a></td></tr>
@@ -36,15 +36,15 @@ include '../../templates/header.tmp.php';
     <tr><td>20</td><td>Philipp</td><td>SKV Mörfelden Titans</td><td>15</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/dm_ergebnisse/DM_TNS_Philipp.pdf'>PDF</a></td></tr>
 </table>
 
-<h3 class="w3-text-secondary">Ergebnisse B-Meisterschaft 2024/25</h1>
+<h3 class="w3-text-secondary">Ergebnisse B-Meisterschaft 2024/25</h3>
 <table class="w3-border w3-border-light-grey w3-table w3-striped">
     <tr class="w3-primary"><th>Platz</th><th>Name</th><th>Team</th><th>Punkte</th><th>Details</th></tr>
     <tr><td>1</td><td>Jakob</td><td>MUH-Bande Jena</td><td>59</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_MUH_Jakob.pdf'>PDF</a></td></tr>
-    <tr><td>2</td><td>Nele</td><td>BTC Heisse Reifen</td><td>56</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_HSR_Nele.pdf'>PDF</a></td></tr>
+    <tr><td>2</td><td>Nele</td><td>BTC Heisse Reifen</td><td>56</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_REI_Nele.pdf'>PDF</a></td></tr>
     <tr><td>3</td><td>Jessica</td><td>SKV Mörfelden Phönix</td><td>51</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_PHX_Jessi.pdf'>PDF</a></td></tr>
     <tr><td>4</td><td>Malte</td><td>TV Lilienthal Moorteufel</td><td>46</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_MRT_Malte.pdf'>PDF</a></td></tr>
     <tr><td>5</td><td>Matteo</td><td>Dresdner Einradtiger</td><td>40</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_EIT_Matteo.pdf'>PDF</a></td></tr>
-    <tr><td>5</td><td>Philipp</td><td>SKV Mörfelden Titans</td><td>40</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_TNS_PeKA.pdf'>PDF</a></td></tr>
+    <tr><td>5</td><td>Philipp</td><td>SKV Mörfelden Titans</td><td>40</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_TNS_Philipp.pdf'>PDF</a></td></tr>
     <tr><td>7</td><td>Clemens</td><td>Dresdner Einradtiger</td><td>39</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_EIT_Clemens.pdf'>PDF</a></td></tr>
     <tr><td>8</td><td>Carla</td><td>Hockey Hawks</td><td>38</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_HAW_Carla.pdf'>PDF</a></td></tr>
     <tr><td>9</td><td>Lea</td><td>MUH-Bande Jena</td><td>37</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_MUH_Lea.pdf'>PDF</a></td></tr>
@@ -62,7 +62,7 @@ include '../../templates/header.tmp.php';
     <tr><td>21</td><td>Kristin</td><td>MUH-Bande Jena</td><td>13</td><td><a target="_blank" class='no w3-text-primary w3-hover-text-secondary' href='/dokumente/tippspiel/bm_ergebnisse/BM_MUH_Kristin.pdf'>PDF</a></td></tr>
 </table>
 
-<h3 class="w3-text-secondary">Punktevergabe</h1>
+<h3 class="w3-text-secondary">Punktevergabe</h3>
 <ul>
     <li>Wird ein Ergebnis in der Gruppenphase korrekt getippt, gibt es dafür 5 Punkte. Ist die richtige Tendenz gegeben, gibt es 
         dafür 1 Punkt. Ein Penaltyschießen wird dabei nicht berücksichtigt.</li>
