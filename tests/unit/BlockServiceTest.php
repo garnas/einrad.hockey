550e963ffba39e49970b771ac6fe888f5996af9f
Ansgar <a.poelking@gmai.com>
2025-06-29 00:36:56 +0200

    Tests erweitert

--- tests/unit/BlockServiceTest.php
+++ tests/unit/BlockServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace unit;
 
+use App\Entity\Team\nTeam;
 use App\Entity\Turnier\Turnier;
 use App\Service\Turnier\BlockService;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -52,4 +53,67 @@ class BlockServiceTest extends TestCase
         );
     }
 
+    public static function provideIsBlockPassend(): array
+    {
+        return [
+            ["turnierblock" => "CDE", "teamblock" => "CD", "passend" => true],
+            ["turnierblock" => "A", "teamblock" => "CD", "passend" => false],
+            ["turnierblock" => "A", "teamblock" => "F", "passend" => false],
+            ["turnierblock" => "A", "teamblock" => "A", "passend" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "CD", "passend" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "EF", "passend" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "AB", "passend" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "A", "passend" => false],
+            ["turnierblock" => "BCDE", "teamblock" => "F", "passend" => false],
+            ["turnierblock" => "DEF", "teamblock" => "F", "passend" => true],
+            ["turnierblock" => "DEF", "teamblock" => "EF", "passend" => true],
+            ["turnierblock" => "DEF", "teamblock" => "BC", "passend" => false],
+            ["turnierblock" => "ABCDEF", "teamblock" => "F", "passend" => true],
+            ["turnierblock" => "ABCDEF", "teamblock" => "CD", "passend" => true],
+
+        ];
+    }
+
+    #[DataProvider("provideIsBlockPassend")]
+    public function testIsBlockPassend(string $turnierblock, string $teamblock, bool $passend)
+    {
+        $turnier = (new Turnier())->setBlock($turnierblock);
+        $teamMock = $this->createMock(nTeam::class);
+        $teamMock->method('getBlock')->willReturn($teamblock);
+        $this->assertEquals(
+            expected: $passend,
+            actual: BlockService::isBlockPassend($turnier, $teamMock)
+        );
+    }
+
+    public static function provideIsTurnierblockHoeher(): array
+    {
+        return [
+            ["turnierblock" => "CDE", "teamblock" => "CD", "hoeher" => false],
+            ["turnierblock" => "A", "teamblock" => "CD", "hoeher" => true],
+            ["turnierblock" => "A", "teamblock" => "F", "hoeher" => true],
+            ["turnierblock" => "A", "teamblock" => "A", "hoeher" => false],
+            ["turnierblock" => "ABC", "teamblock" => "DE", "hoeher" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "F", "hoeher" => true],
+            ["turnierblock" => "BCDE", "teamblock" => "A", "hoeher" => false],
+            ["turnierblock" => "CDE", "teamblock" => "F", "hoeher" => true],
+            ["turnierblock" => "DE", "teamblock" => "F", "hoeher" => true],
+            ["turnierblock" => "F", "teamblock" => "F", "hoeher" => false],
+            ["turnierblock" => "F", "teamblock" => "A", "hoeher" => false],
+
+        ];
+    }
+
+    #[DataProvider("provideIsTurnierblockHoeher")]
+    public function testIsTurnierBlockHigher(string $turnierblock, string $teamblock, bool $hoeher)
+    {
+        $turnier = (new Turnier())->setBlock($turnierblock);
+        $teamMock = $this
+            ->createMock(nTeam::class);
+        $teamMock->method('getBlock')->willReturn($teamblock);
+        $this->assertEquals(
+            expected: $hoeher,
+            actual: BlockService::isTurnierBlockHigher($turnier, $teamMock)
+        );
+    }
 }
