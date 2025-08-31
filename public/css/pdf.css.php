74eb59d305bff3e24b7ff4d9a4d586b23377dbf8
Ansgar <a.poelking@gmai.com>
2025-08-31 15:24:53 +0200

    Quittungen CSS Fix

--- public/css/pdf.css.php
+++ public/css/pdf.css.php
@@ -5,7 +5,7 @@ html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}
 html{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}
 /* End extract */
 
-html,body{font-family:Verdana,sans-serif;font-size:<?=$font_size?>px;line-height:1}html{overflow-x:hidden}
+html,body{font-family:Verdana,sans-serif;font-size:<?=$font_size ?? "12"?>px;line-height:1}html{overflow-x:hidden}
 
 h1{font-size:30px}h2{font-size:24px}h3{font-size:24px}h4{font-size:20px}h5{font-size:18px}h6{font-size:16px}.w3-serif{font-family:serif}
 
@@ -14,7 +14,7 @@ h1,h2,h3,h4,h5,h6{font-family:"Segoe UI",Arial,sans-serif;font-weight:400;margin
 .w3-table {border-collapse:collapse;border-spacing:0;width:100%;display:table}.w3-table-all{border:1px solid #ccc}
 
 .w3-hoverable tbody tr:hover,.w3-ul.w3-hoverable li:hover{background-color:#ccc}.w3-centered tr th,.w3-centered tr td{text-align:center}
-.w3-table td,.w3-table th,.w3-table-all td,.w3-table-all th{padding:<?=$padding?> 4px;display:table-cell;vertical-align:middle}
+.w3-table td,.w3-table th,.w3-table-all td,.w3-table-all th{padding:<?=$padding ?? "3"?> 4px;display:table-cell;vertical-align:middle}
 .w3-table th:first-child,.w3-table td:first-child,.w3-table-all th:first-child,.w3-table-all td:first-child{padding-left:16px}
 .w3-center {text-align:center}
 .w3-left-align{text-align:left!important}
