05d2c6bdf5308b21e77b73994348a2420ab0ad85
Ansgar <a.poelking@gmai.com>
2025-10-12 20:22:27 +0200

    Usability verbessert: Ganze Zeilen clickable in Rangtabellen

--- templates/tabellen/desktop_rangtabelle.tmp.php
+++ templates/tabellen/desktop_rangtabelle.tmp.php
@@ -20,12 +20,12 @@
         <?php $nthcolor = $counter % 2 == 0 ? '' : 'w3-light-grey'; ?>
         
         <!-- Kopfzeile fuer das Team -->
-        <div id="large-rang-head-<?=$key?>" class="w3-row <?=$nthcolor?> w3-border-bottom w3-border-grey">
+        <div style="cursor: pointer;" onclick="show_large_results('rang', <?=$key?>)" id="large-rang-head-<?=$key?>" class="w3-row <?=$nthcolor?> w3-border-bottom w3-border-grey w3-hover-text-secondary">
             <div class="w3-col w3-right w3-padding-8 w3-center" style="width: 42px;">
                 <? if (!empty($zeile['details'])): ?>
-                    <span onclick="show_large_results('rang', <?=$key?>)" style="cursor: pointer;">
-                        <span id="large-rang-icon-show-<?=$key?>" class="material-icons w3-text-primary w3-hover-text-secondary" style="display:block">arrow_drop_down</span>
-                        <span id="large-rang-icon-hide-<?=$key?>" class="material-icons w3-text-primary w3-hover-text-secondary" style="display:none">arrow_drop_up</span>
+                    <span>
+                        <span id="large-rang-icon-show-<?=$key?>" class="material-icons w3-text-primary" style="display:block">arrow_drop_down</span>
+                        <span id="large-rang-icon-hide-<?=$key?>" class="material-icons w3-text-primary" style="display:none">arrow_drop_up</span>
                     </span>
                 <? endif; ?>
             </div>
@@ -42,17 +42,19 @@
         <!-- Details zu den Turnieren des Teams -->
         <?php if (!empty($zeile['details'])): ?>
             <?php foreach ($zeile['details'] as $dey => $ergebnis): ?>
-                <div class="large-rang-result-<?=$key?> w3-row <?=$nthcolor?> <?=$saison != $ergebnis['saison'] ? 'w3-text-grey' : 'w3-text-primary'?>" style="display: none;">
-                    <div class="w3-col l2 m2 w3-padding-8 w3-right-align"><?=date_format(date_create($ergebnis['datum']), "d.m.y")?></div>
-                    <div class="w3-col l2 m2 w3-padding-8"><?=$ergebnis['tblock']?></div>
-                    <div class="w3-col l3 m3 w3-padding-8"><?=$ergebnis['ort']?></div>
-                    <div class="w3-col l3 m3 w3-padding-8 w3-right-align">Platz <?=$ergebnis['platz']?> / <?=$ergebnis['teilnehmer']?></div>
-                    <div class="w3-col l2 m2 w3-padding-8 w3-right-align">
-                        <a href="ergebnisse.php?saison=<?=$ergebnis['saison']?>#<?=$ergebnis['turnier_id']?>" class="no w3-hover-text-secondary"> 
-                            <?=number_format($ergebnis['ergebnis'] ?: 0, 0, ",", ".")?>
-                        </a>
+                <a href="ergebnisse.php?saison=<?=$ergebnis['saison']?>#<?=$ergebnis['turnier_id']?>" class="no">
+                    <div class="w3-hover-text-secondary large-rang-result-<?= $key ?> w3-row <?= $nthcolor ?> <?= $saison != $ergebnis['saison'] ? 'w3-text-grey' : 'w3-text-primary' ?>"
+                         style="display: none;">
+                        <div class="w3-col l2 m2 w3-padding-8 w3-right-align"><?= date_format(date_create($ergebnis['datum']), "d.m.y") ?></div>
+                        <div class="w3-col l2 m2 w3-padding-8"><?= $ergebnis['tblock'] ?></div>
+                        <div class="w3-col l3 m3 w3-padding-8"><?= $ergebnis['ort'] ?></div>
+                        <div class="w3-col l3 m3 w3-padding-8 w3-right-align">Platz <?= $ergebnis['platz'] ?>
+                            / <?= $ergebnis['teilnehmer'] ?></div>
+                        <div class="w3-col l2 m2 w3-padding-8 w3-right-align">
+                            <?= number_format($ergebnis['ergebnis'] ?: 0, 0, ",", ".") ?>
+                        </div>
                     </div>
-                </div>
+                </a>
             <?php endforeach; ?>    
         <?php endif; ?>
         <?php $counter++; ?>
