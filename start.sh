2e4a91ed418e954059b2e3222ac1980f9c28292f
Ansgar Pölking <ansgar@einrad.hockey>
2026-01-18 00:39:40 +0100

    Linux .....

---
+++ start.sh
@@ -0,0 +1,30 @@
+sudo su -
+sudo chmod -R 777 .
+sudo chmod -R 777 /var/lib/php/sessions
+apt-get install php
+
+sudo apt -y install php8.3-mysql
+sudo apt -y install php8.3-tokenizer
+sudo apt -y install php8.3-sysvsem
+sudo apt -y install php8.3-sockets
+sudo apt -y install php8.3-readline
+sudo apt -y install php8.3-posix
+sudo apt -y install php8.3-phar
+sudo apt -y install php8.3-mbstring
+sudo apt -y install php8.3-intl
+sudo apt -y install php8.3-iconv
+sudo apt -y install php8.3-ftp
+sudo apt -y install php8.3-fileinfo
+sudo apt -y install php8.3-ffi
+sudo apt -y install php8.3-exif
+sudo apt -y install php8.3-curl
+sudo apt -y install php8.3-ctype
+sudo apt -y install php8.3-calendar
+sudo apt -y install php8.3-pdo
+sudo apt -y install php8.3-mysql
+sudo apt -y install php8.3-gd
+sudo apt -y install php8.3-exif
+sudo apt -y install php8.3-mbstring
+sudo apt -y install php8.3-xsl
+sudo apt -y install php8.3-zip
+sudo apt -y install php8.3-xdebug
