662a25d41327ac7b5667673f2c9138ee1411e940
Philipp Gross <philipp@einrad.hockey>
2026-01-27 14:27:01 +0100

    utilized IntlDateFormatter

--- public/liga/foerdermittel.php
+++ public/liga/foerdermittel.php
@@ -9,30 +9,19 @@ use App\Enum\NeuigkeitArt;
 require_once '../../init.php';
 
 $saison = (isset($_GET['saison'])) ? (int)$_GET['saison'] : Config::SAISON;
-$neuigkeiten = NeuigkeitRepository::get()->findByType(NeuigkeitArt::FOERDERMITTEL);
+$foerdermittel = NeuigkeitRepository::get()->findByType(NeuigkeitArt::FOERDERMITTEL);
 
-// gruppiere die neuigkeiten nach jahr und monaten
-$neuigkeiten_nach_jahr = [];
-foreach ($neuigkeiten as $neuigkeit) {
-    $jahr = $neuigkeit->getZeit()->format('Y');
-    $monat = $neuigkeit->getZeit()->format('m');
-    $neuigkeiten_nach_jahr[$jahr][$monat][] = $neuigkeit;
+$foerdermittel_nach_jahr = [];
+
+// gruppiere die foerdermittel nach jahr und monaten
+$formatter = new IntlDateFormatter('de_DE', pattern: 'MMMM');
+foreach ($foerdermittel as $mittel) {
+    $jahr = $mittel->getZeit()->format('Y');
+    $monat = $formatter->format($mittel->getZeit());
+    $foerdermittel_nach_jahr[$jahr][$monat][] = $mittel;
 }
 
-$monate = [
-    '01' => 'Januar',
-    '02' => 'Februar',
-    '03' => 'März',
-    '04' => 'April',
-    '05' => 'Mai',
-    '06' => 'Juni',
-    '07' => 'Juli',
-    '08' => 'August',
-    '09' => 'September',
-    '10' => 'Oktober',
-    '11' => 'November',
-    '12' => 'Dezember'
-];
+db::debug($foerdermittel_nach_jahr);
 
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
@@ -57,14 +46,14 @@ include '../../templates/header.tmp.php'; ?>
             <div class="w3-stretch w3-container w3-primary">
                 <h3>Zeitleiste</h3>
             </div>
-            <?php foreach ($neuigkeiten_nach_jahr as $jahr => $neuigkeiten_nach_monat): ?>
+            <?php foreach ($foerdermittel_nach_jahr as $jahr => $neuigkeiten_nach_monat): ?>
                 <div class="w3-section">
                     <h4 class="w3-text-primary"><?= $jahr ?></h4>
                     <ul class="w3-ul w3-hoverable">
                         <?php foreach ($neuigkeiten_nach_monat as $monat => $neuigkeiten): ?>
                             <li class="w3-padding-small">
                                 <a class="" href="#<?= $jahr ?><?= $monat ?>" style="display:block; color:inherit; text-decoration:none;">
-                                    <?= $monate[$monat] ?>
+                                    <?= $monat ?>
                                 </a>
                             </li>
                         <?php endforeach; ?>
@@ -75,9 +64,9 @@ include '../../templates/header.tmp.php'; ?>
     </div>
 
     <div class="w3-col l9 m8">
-        <?php foreach ($neuigkeiten_nach_jahr as $jahr => $neuigkeiten_nach_monat): ?>
+        <?php foreach ($foerdermittel_nach_jahr as $jahr => $neuigkeiten_nach_monat): ?>
             <?php foreach ($neuigkeiten_nach_monat as $monat => $neuigkeiten): ?>
-                <h2 id="<?= $jahr ?><?= $monat ?>" class="w3-text-primary w3-border-bottom w3-border-grey"><?= $monate[$monat] ?> <?= $jahr ?></h2>
+                <h2 id="<?= $jahr ?><?= $monat ?>" class="w3-text-primary w3-border-bottom w3-border-grey"><?= $monat ?> <?= $jahr ?></h2>
                 <?php foreach ($neuigkeiten as $neuigkeit): ?>
                     <?php include '../../templates/neuigkeiten/neuigkeit.tmp.php'; ?>
                 <?php endforeach; ?>
--- public/liga/neuigkeiten.php
+++ public/liga/neuigkeiten.php
@@ -10,28 +10,15 @@ require_once '../../init.php';
 $saison = (isset($_GET['saison'])) ? (int)$_GET['saison'] : Config::SAISON;
 $neuigkeiten = NeuigkeitRepository::get()->findAll();
 
-// gruppiere die neuigkeiten nach jahr und monaten
 $neuigkeiten_nach_jahr = [];
+
+// gruppiere die neuigkeiten nach jahr und monaten
+$formatter = new IntlDateFormatter('de_DE', pattern: 'MMMM');
 foreach ($neuigkeiten as $neuigkeit) {
     $jahr = $neuigkeit->getZeit()->format('Y');
-    $monat = $neuigkeit->getZeit()->format('m');
+    $monat = $formatter->format($neuigkeit->getZeit());
     $neuigkeiten_nach_jahr[$jahr][$monat][] = $neuigkeit;
 }
-
-$monate = [
-    '01' => 'Januar',
-    '02' => 'Februar',
-    '03' => 'März',
-    '04' => 'April',
-    '05' => 'Mai',
-    '06' => 'Juni',
-    '07' => 'Juli',
-    '08' => 'August',
-    '09' => 'September',
-    '10' => 'Oktober',
-    '11' => 'November',
-    '12' => 'Dezember'
-];
 
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////LAYOUT///////////////////////////////////
@@ -58,7 +45,7 @@ include '../../templates/header.tmp.php'; ?>
                         <?php foreach ($neuigkeiten_nach_monat as $monat => $neuigkeiten): ?>
                             <li class="w3-padding-small">
                                 <a class="" href="#<?= $jahr ?><?= $monat ?>" style="display:block; color:inherit; text-decoration:none;">
-                                    <?= $monate[$monat] ?>
+                                    <?= $monat ?>
                                 </a>
                             </li>
                         <?php endforeach; ?>
@@ -71,7 +58,7 @@ include '../../templates/header.tmp.php'; ?>
     <div class="w3-col l9 m8">
         <?php foreach ($neuigkeiten_nach_jahr as $jahr => $neuigkeiten_nach_monat): ?>
             <?php foreach ($neuigkeiten_nach_monat as $monat => $neuigkeiten): ?>
-                <h2 id="<?= $jahr ?><?= $monat ?>" class="w3-text-primary w3-border-bottom w3-border-grey"><?= $monate[$monat] ?> <?= $jahr ?></h2>
+                <h2 id="<?= $jahr ?><?= $monat ?>" class="w3-text-primary w3-border-bottom w3-border-grey"><?= $monat ?> <?= $jahr ?></h2>
                 <?php foreach ($neuigkeiten as $neuigkeit): ?>
                     <?php include '../../templates/neuigkeiten/neuigkeit.tmp.php'; ?>
                 <?php endforeach; ?>
