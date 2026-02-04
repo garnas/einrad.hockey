f895912a25d417eba4a20bd1903f084af840147b
Philipp Gross <philipp@einrad.hockey>
2026-02-04 10:05:48 +0000

    moved files to localhost

---
+++ _localhost/code_archive/abstimmung_foerdermittel/classes/abstimmung.class.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * Class Abstimmung
+ *
+ * Handling der Abstimmung zum Saisonwechsel
+ *
+ * Es gibt zwei Tabellen: abstimmung_teams ist eine Tabelle mit allen Teams die bisher Abgestimmt haben und wichtigen
+ * Metadaten. abstimmung_ergebnisse ist eine Tabelle mit verschlüsselter TeamID und Stimme des Teams. Durch Verschlüsselung
+ * der TeamID mit dem Passwort des Teams, ist die Abstimmung anonym und ein Team kann seine Stimme im Nachhinein noch ändern.
+ */
+class Abstimmung
+{
+    public const OPTIONS = [
+        "Ja" => "Ja",
+        "Nein" => "Nein",
+        "Keine_Angabe" => "Keine Angabe",
+    ];
+
+    public const OPTIONS_WICHTIGKEIT = [
+        "Trainingsmaterialien" => "Trainingsmaterialien für den A- und B-Kader",
+        "Trikots" => "Trikots und/oder Trainingsjacken für den A- und B-Kader",
+        "Hallenkosten" => "Hallenkosten bei Einradhockey-Veranstaltungen (Trainingslager, 24 Stunden Turniere etc.)",
+        "Verpflegung" => "Verpflegung für Teilnehmende bei Einradhockey-Veranstaltungen",
+        "Uebernachtungskosten" => "Übernachtungskosten bei Einradhockey-Veranstaltungen",
+        "Aufwandsentschaedigung" => "Aufwandsentschädigung von Übungsleitern bei Einradhockey-Veranstaltungen (z.B. Trainingslager)"
+    ];
+
+    public static function selected(string $name, string $value, ?string $value_chosen): string
+    {
+        if ($value_chosen != null) {
+            return ($value === $value_chosen) ? "selected checked" : "";
+        }
+        return (($_POST[$name] ?? "") == $value) ? "selected checked" : "";
+    }
+
+    /**
+     * Frühstmöglicher Zeitpunkt der Stimmabgabe
+     */
+    public const BEGINN = "17.03.2025 00:00";
+    /**
+     * Letztmöglicher Zeitpunkt der Stimmabgabe
+     */
+    public const ENDE = "13.04.2025 23:59";
+    /**
+     * Verschlüsselungsverfahren für die TeamIDs
+     */
+    private const CIPHER = 'AES-256-CBC';
+    /**
+     * Verhindert, dass bei einer neuen Abstimmung der gleiche Crypt bei gleichen Passwort und TeamID entsteht.
+     * Muss für jede Liga-Abstimmung erneuert werden.
+     */
+    private const IV = 'ZUEvrHAovY29t/bA';
+    /**
+     * @var array|string[] Daten aus der DB-Tabelle abstimmung_teams
+     */
+    public array $team;
+    /**
+     * @var int Eindeutige ligaweite TeamID
+     */
+    public int $team_id;
+    /**
+     * @var string Passwort-Hash, welcher für das Team bei erstmaliger Abstimmung hinterlegt war.
+     */
+    public string $passwort_hash;
+
+    /**
+     * Abstimmung constructor.
+     *
+     * Erstellt eine Klasse für die Abstimmung eines Teams und initiert wichtige Variablen.
+     * Enthält statische Methoden für die Einsicht des Abstimmungsergebnisses.
+     *
+     * @param $team_id
+     */
+    public function __construct($team_id)
+    {
+        $this->team_id = $team_id;
+        if (!Team::is_ligateam($team_id)) {
+            trigger_error("Ungültige Team-ID", E_USER_ERROR);
+        }
+        // Details aus abstimmung_teams bekommen
+        $sql = "
+                SELECT *
+                FROM abstimmung_teams
+                WHERE team_id = ?
+                ";
+        $this->team = db::$db->query($sql, $this->team_id)->esc()->fetch_row();
+
+        // Beim ersten Mal Abstimmen den für die Verschlüsselung benutzen Passwort-Hash speichern.
+        if (empty($this->team)) {
+            $this->passwort_hash = (new Team($team_id))->details['passwort'];
+        } else {
+            $this->passwort_hash = $this->team['passwort'];
+        }
+
+    }
+
+    /**
+     * Optional: Entschlüsselung einer TeamID
+     *
+     * @param $passwort
+     * @param $crypt
+     * @return string
+     */
+    public function crypt_to_teamid($passwort, $crypt): string
+    {
+        // Verwendete password-based key derivation, das User-Passwort kein geeigner Key ist zum verschlüsseln.
+        $key = hash_pbkdf2("sha256", $passwort, $this->passwort_hash, 8000);
+        return openssl_decrypt($crypt, self::CIPHER, $key, 0, self::IV);
+    }
+
+    /**
+     * Verschlüsselung der TeamID
+     *
+     * @param string $passwort
+     * @return string
+     */
+    public function teamid_to_crypt(string $passwort): string
+    {
+        // Verwendete password-based key derivation, das User-Passwort kein geeigner Key ist zum verschlüsseln.
+        $key = hash_pbkdf2("sha256", $passwort, $this->passwort_hash, 8000);
+        return openssl_encrypt($this->team_id, self::CIPHER, $key, 0, self::IV);
+    }
+
+    /**
+     * Gibt die einem $crypt zugeordneten Stimme aus.
+     *
+     * @param string $crypt
+     * @return array
+     */
+    public function get_stimme(string $crypt): array
+    {
+        $sql = "
+            SELECT stimme 
+            FROM abstimmung_ergebnisse
+            WHERE crypt = ?
+            ";
+        $stimme =  db::$db->query($sql, $crypt)->fetch_one() ?? '';
+        return json_decode($stimme, associative: true) ?? [];
+    }
+
+    /**
+     * Schreibt eine Stimme in die Datenbank.
+     * Wenn die Stimme bereits gezählt wurde, dann wird diese aktualisiert.
+     * Erstellt Log und Affirm-Meldung.
+     *
+     * @param $stimme
+     * @param $crypt // Verschlüsselte TeamID
+     */
+    public function set_stimme($stimme, $crypt): void
+    {
+        // Validierung
+        if (
+            (empty($this->team) xor empty($this->get_stimme($crypt))
+        )){
+            Html::error("Fehler, bitte melde dich bei " . Html::mailto(Env::TECHNIKMAIL), esc:false);
+            Helper::log("abstimmung.log", "Fehler: $this->team_id | $crypt");
+            return;
+        }
+
+        if (empty($this->team)) { // Team stimmt zum ersten Mal ab.
+            $sql = "
+                INSERT INTO abstimmung_teams (team_id, passwort)
+                VALUES ($this->team_id, ?)";
+            db::$db->query($sql, $this->passwort_hash)->log(true);
+            $sql = "
+                INSERT INTO abstimmung_ergebnisse (stimme, crypt) 
+                VALUES (?, ?)
+                ";
+            db::$db->query($sql, $stimme, $crypt)->log(true);
+            Helper::log("abstimmung.log", "$this->team_id hat seine Stimme abgegeben");
+            Html::info("Dein Team hat erfolgreich abgestimmt. Vielen Dank!");
+        } else { // Team korrigiert seine Stimme
+            $sql = "
+                UPDATE abstimmung_ergebnisse
+                SET stimme = ?
+                WHERE crypt = ?
+                ";
+            db::$db->query($sql, $stimme, $crypt)->log(true);
+            $sql = "
+                UPDATE abstimmung_teams
+                SET aenderungen = aenderungen + 1
+                WHERE team_id = $this->team_id
+                ";
+            db::$db->query($sql)->log(true);
+            Helper::log("abstimmung.log", "$this->team_id hat seine Stimme geändert");
+            Html::info("Dein Team hat erfolgreich neu abgestimmt. Vielen Dank!");
+        }
+    }
+
+    /**
+     * Gibt das Gesamt-Ergebnis der Abstimmung aus.
+     *
+     *
+     * @param int $min Minimale anzahl an Stimmen bis zur Veröffentlichung
+     * @return array Array mit den Ergebnissen der Abstimmung
+     */
+    public static function get_ergebnisse(int $min = 0): array
+    {
+        $sql = 'select * from abstimmung_ergebnisse';
+        $stimmen = db::$db->query($sql)->list('stimme');
+
+        $ergebnisse = array();
+        foreach ($stimmen as $key => $stimme) {
+            $decode = json_decode($stimme, associative: true);
+            foreach ($decode as $key => $value) {
+                $ergebnisse[$key][$value] = ($ergebnisse[$key][$value] ?? 0) + 1;
+            }
+        }
+
+        // Wenn die Anzahl der Stimmen unter dem Minimum ist, dann wird nichts ausgegeben.
+        if (count($stimmen) < $min) {
+            return [];
+        }
+        
+        return $ergebnisse;
+    }
+
+    /**
+     * Gibt die Anzahl der abgegebenen Stimmen zurück.
+     *
+     * @return int
+     */
+    public static function get_anzahl_stimmen(): int
+    {
+        $sql = 'select count(*) from abstimmung_ergebnisse';
+        return db::$db->query($sql)->fetch_one();
+    }
+
+}
---
+++ _localhost/code_archive/abstimmung_foerdermittel/teamcenter/tc_abstimmung.php
@@ -0,0 +1,197 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LOGIK////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+use App\Repository\Team\TeamRepository;
+
+require_once '../../init.php';
+Helper::ensure_no_request_logging();
+
+require_once '../../logic/session_team.logic.php'; //Auth
+require_once '../../logic/abstimmung.logic.php';
+$teams = TeamRepository::get()->activeLigaTeams();
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LAYOUT///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+include '../../templates/header.tmp.php';
+?>
+    <div class="w3-card-4 w3-panel">
+
+        <h1 class="w3-text-primary">Abstimmung Fördermittel</h1>
+        <?php 
+            if (time() < $beginn):
+                Html::message('notice',
+                "Die Abstimmung startet hier am " . date("d.m.Y", $beginn) . " und endet am "
+                . date("d.m.Y \u\m H:i", $abschluss) . " Uhr. ",
+                Null);
+            endif;
+        ?>
+        <p><strong>Informationen</strong></p>
+        <p>Wie ihr bereits gehört habt, wurde der Ligabeitrag unter anderem erhöht, um Projekte wie die Förderung von Einradhockey-Initiativen zu unterstützen. Bislang haben wir vier Anträge erhalten. Um diese Mittel gerecht und transparent zu verteilen, haben wir ein vorläufiges Budget von 3.000,00 € festgelegt, über dessen Verteilung ihr mitentscheiden sollt.</p>
+        <p>Alle derzeit eingegangenen Anträge können gefördert werden, und es besteht weiterhin die Möglichkeit, weitere Anträge zu stellen, wenn ihr ein neues Projekt einbringen möchtet.</p>
+        <p>Jedes Team hat eine Stimme bei der Umfrage, sodass wir sicherstellen können, dass die Entscheidung gemeinsam getroffen wird.</p>
+        <p>Bitte nehmt euch einen Moment Zeit, um an der Umfrage teilzunehmen und uns eure Meinung zu den Fördermaßnahmen mitzuteilen. Die Umfrage wird bis zum  <?= date("d.m.Y", $abschluss) ?> offen sein.</p>
+
+        <?php if (time() > $beginn && time() < $abschluss): ?>
+            <p><strong>Verbleibende Zeit</strong></p>
+            <?php Html::countdown(strtotime(Abstimmung::ENDE)) ?>
+        <?php endif; ?>
+
+        <p><strong>Fragen</strong></p>
+        <p>
+            <?= Html::link(Nav::LINK_FORUM, "Discord", "true", "chat") ?>
+            oder <?= Html::mailto(Env::LAMAIL) ?>
+        </p>
+
+    </div>
+
+    <!-- Nach Beginn der Abstimmung -->
+    <?php if (time() > $beginn && time() < $abschluss): ?>
+        <!-- Informationstext für die Stimmeinsicht -->
+        <div class="w3-card-4 w3-panel">
+            <h2 class="w3-text-primary">Status</h2>
+
+            <?php if (empty($abstimmung->team)): ?>
+                <!-- Noch nicht abgestimmt hinterlegt -->
+                <p class="w3-text-secondary">Es ist keine Stimme für dein Team hinterlegt.</p>
+            <?php else: ?>
+                <p class="w3-text-green">Es wurde eine Stimme für dein Team hinterlegt.
+                                         (<?= $abstimmung->team['aenderungen'] ?> mal geändert)</p>
+                <?php if (empty($einsicht)): ?>
+                    <!-- Formular zur Stimmeinsicht -->
+                    <form method="post">
+                        <p>
+                            <!-- Passwort -->
+                            <label for="passwort_einsicht" style="cursor: pointer;">
+                                Bitte Teamcenter-Passwort eingeben, um deine Stimme einzusehen:
+                            </label>
+                            <input required
+                                   type="password"
+                                   name="passwort"
+                                   id="passwort_einsicht"
+                                   placeholder="Passwort eingeben"
+                                   class="w3-input"
+                            >
+                        </p>
+                        <p>
+                            <button type="submit"
+                                    name="stimme_einsehen"
+                                    class="w3-primary w3-button"
+                            >
+                                <i class="material-icons">info</i>
+                                Stimme einsehen
+                            </button>
+                        </p>
+                    </form>
+                <?php else: ?>
+                    <p id="stimme">Deine Stimme wird unten im Formular angezeigt.</p>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (time() > $beginn && time() < $abschluss): ?>
+        <!-- Formular zur Stimmabgabe -->
+        <div class="w3-card-4 w3-panel">
+            <h2 class="w3-text-primary"><?= (empty($abstimmung->team)) ? 'Jetzt abstimmen' : 'Stimme ändern' ?></h2>
+            <form method="post">
+                <p>Bitte bewertet die folgenden Fördermaßnahmen nach ihrer Wichtigkeit für die Liga (5 = sehr wichtig, 1 = weniger wichtig):</p>
+                <?php foreach (Abstimmung::OPTIONS_WICHTIGKEIT as $id => $option): ?>
+                    <p class="w3-hover-text-primary">
+                        <!-- Erste Antwortmöglichkeit -->
+                        <label style="cursor: pointer;" for="<?= $id ?>"><?= $option ?></label>
+                        <select
+                                id="<?= $id ?>"
+                                name="<?= $id ?>"
+                                class="w3-select w3-border w3-border-primary">
+                            <option <?= Abstimmung::selected(name: $id, value: "0", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="0">Enthaltung</option>
+                            <option <?= Abstimmung::selected(name: $id, value: "1", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="1">1 - weniger wichtig</option>
+                            <option <?= Abstimmung::selected(name: $id, value: "2", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="2">2</option>
+                            <option <?= Abstimmung::selected(name: $id, value: "3", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="3">3</option>
+                            <option <?= Abstimmung::selected(name: $id, value: "4", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="4">4</option>
+                            <option <?= Abstimmung::selected(name: $id, value: "5", value_chosen: $einsicht[$id] ?? "") ?>
+                                    value="5">5 - sehr wichtig</option>
+                        </select>
+                    </p>
+                <?php endforeach; ?>
+                <p>Weitere Ideen und Anmerkungen:</p>
+                <textarea maxlength="1000" name="Weiteres" class="w3-input w3-border w3-border-primary"><?=$einsicht['Weiteres'] ?? ""?></textarea>
+                <hr>
+                <p>Seid Ihr damit einverstanden, dass die jährlichen Beiträge der Mitglieder der Liga für Fördermaßnahmen eingesetzt werden, die zur Förderung der deutschen Einradhockeyliga und des Sports beitragen?</p>
+                <?php foreach (Abstimmung::OPTIONS as $id => $option): ?>
+                    <p class="w3-hover-text-primary">
+                        <!-- Erste Antwortmöglichkeit -->
+                        <input required
+                               <?= Abstimmung::selected(name: "option", value: $id, value_chosen: $einsicht["option"] ?? null) ?>
+                               type="radio"
+                               name="option"
+                               id="<?= $id ?>"
+                               value="<?= $id ?>"
+                               class="w3-radio"
+                        >
+                        <label style="cursor: pointer;" for="<?= $id ?>"><?= $option ?></label>
+                    </p>
+                <?php endforeach; ?>
+                <hr>
+                <p>
+                    <!-- Passwort -->
+                    <label for="passwort" style="cursor: pointer;">
+                        Bitte Teamcenter-Passwort eingeben, um abzustimmen:
+                    </label>
+                    <input required
+                           type="password"
+                           name="passwort"
+                           id="passwort"
+                           placeholder="Passwort eingeben"
+                           class="w3-input"
+                    >
+                </p>
+                <p>
+                    <button type="submit" name="abgestimmt" class="w3-block w3-primary w3-button">
+                        <i class="material-icons">how_to_vote</i>
+                        <?php if (empty($abstimmung->team)): ?>
+                            Stimme abgeben
+                        <?php else: ?>
+                            Stimme ändern
+                        <?php endif; ?>
+                    </button>
+                </p>
+            </form>
+        </div>
+        <div class="w3-card-4 w3-panel">
+
+            <!-- Hinweise zur Abstimmung -->
+            <h2 class="w3-text-primary">Hinweise zur Abstimmung</h2>
+            <p>
+                <i>
+                    Die Abstimmung ist anonym - die Stimmzuordnung wird mit eurem Teamcenter-Passwort verschlüsselt.
+                    Eine Einsicht und Änderung eurer abgegebenen Stimme ist im Nachhinein möglich.
+                </i>
+            </p>
+            <p>
+                <i>
+                    Dafür muss immer das Teamcenter-Passwort eingegeben werden, mit welchem ihr das erste Mal abgestimmt habt
+                    (unabhängig davon, ob es danach geändert wurde). Ohne euer Passwort ist eine Einsicht oder Änderung eurer
+                    Stimme nicht mehr möglich.
+                </i>
+            </p>
+        </div>
+
+    <?php endif; ?>
+
+    <!-- NACH DER ABSTIMMUNG -->
+    <?php if (time() > $abschluss): ?>
+        <div class="w3-panel w3-light-grey">
+            <p class="w3-large">Die Abstimmung ist beendet.</p>
+            <p><?= Html::link('../liga/abstimmung.php', "Zu den Ergebnissen", icon:"info") ?></p>
+        </div>
+    <?php endif; ?>
+
+<?php
+include '../../templates/footer.tmp.php';
