62f869da75f9a3df4a51d0e7bd01c2e01399ef23
Ansgar Pölking <ansgar@einrad.hockey>
2026-02-13 19:10:45 +0100

    Ausstehende DB-Changes für einrad.hockey

--- changes.sql
+++ changes.sql
@@ -1,1 +1,12 @@
-ALTER TABLE `neuigkeiten` ADD `art` ENUM('neuigkeit','foerdermittel') NOT NULL DEFAULT 'neuigkeit' AFTER `aktiv`; 
+ALTER TABLE `neuigkeiten`
+    ADD `art` ENUM ('neuigkeit','foerdermittel') NOT NULL DEFAULT 'neuigkeit' AFTER `aktiv`;
+
+CREATE TABLE IF NOT EXISTS `db_localhost`.`spieler_statistik`
+(
+    `id`         INT      NOT NULL AUTO_INCREMENT,
+    `date`       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `saison`     INT      NOT NULL,
+    `geschlecht` ENUM ('m', 'w', 'd'),
+    `anzahl`     INT      NOT NULL,
+    PRIMARY KEY (`id`)
+);
