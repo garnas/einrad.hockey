5695b3f63ff1b87404ac0c47943088d9bdabed39
Ansgar Pölking <ansgar@einrad.hockey>
2026-08-16 14:24:25 +0200

    PHP Fix Session Init Settings

--- init.php
+++ init.php
@@ -12,8 +12,9 @@ ini_set('session.use_cookies', '1');
 ini_set('session.use_only_cookies', '1');
 ini_set('session.use_strict_mode', '1');
 ini_set('session.cookie_httponly', '1');
-ini_set('session.cookie_samesite ', '"Strict"');
+ini_set('session.cookie_samesite', 'Strict');
 ini_set('session.cookie_secure', '1');
+ini_set('session.gc_maxlifetime', '7200'); // Muss zu session.cookie_lifetime passen
 // https://www.php.net/manual/de/session.security.ini.php
 // https://www.php.net/manual/de/features.session.security.management.php
 
