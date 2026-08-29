8ad1f17e6c291a2b633747102a00e4b081abcb38
Ansgar Pölking <ansgar@einrad.hockey>
2026-08-29 14:38:36 +0200

    Hinweis hinzugefügt.

--- templates/spielplan/spielplan_titel.tmp.php
+++ templates/spielplan/spielplan_titel.tmp.php
@@ -42,11 +42,3 @@
         <?php Html::message('notice', $spielplan->get_penalty_warnung(), 'Penalty', false) ?>
     </div>
 <?php } // endif?>
-
-<?php
-Html::message(
-    "notice",
-    "Turniertabelle wird noch nach dem alten Modus von Saison 2025/2026 berechnet.",
-    "Achtung",
-);
-?>
--- templates/spielplan/spielplan_turniertabelle.tmp.php
+++ templates/spielplan/spielplan_turniertabelle.tmp.php
@@ -1,4 +1,10 @@
 <!-- ABSCHLUSSTABELLE -->
+<?php Html::message(
+    "notice",
+    "Turniertabellen werden noch nach dem alten Modus von Saison 2025/2026 berechnet.",
+    "Achtung",
+); ?>
+
 <h2 class="w3-text-secondary w3-margin-top">Tabelle</h2>
 <div class="w3-responsive w3-card">
     <table class="w3-table w3-centered w3-striped">
