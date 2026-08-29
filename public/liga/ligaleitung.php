87227ed9e8a9e129ddafa74e3553046a66abf0f2
Philipp <philipp@einrad.hockey>
2026-08-29 21:07:16 +0000

    LA Bild entfernt

--- public/liga/ligaleitung.php
+++ public/liga/ligaleitung.php
@@ -31,11 +31,6 @@ include '../../templates/header.tmp.php'; ?>
     </p>
 </div>
 
-<div class="w3-section w3-center">
-    <div><img src="<?= Env::BASE_URL ?>/bilder/ligaausschuss.jpg" alt="Ligaausschuss" style="width: 100%; max-width: 600px"></div>
-    <div><i class="w3-text-grey" style="display: block; margin: auto; width: 100%; max-width:600px">Ligaausschuss der Saison <?= Html::get_saison_string() ?>: Larissa, Matthias (ausgeschieden), Max, Malte, Janina, Fin (von links nach rechts)</i></div>
-</div>
-
 <div class="w3-section">
     <?php foreach (LigaLeitung::get_all('ligaausschuss') as $la): ?>
         <div class="w3-row">
